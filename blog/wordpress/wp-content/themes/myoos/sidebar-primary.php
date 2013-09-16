ba1f51061a8675109c24c05aa60836a209a06d43
r23 <info@oos-shop.de>
2013-09-16 18:54:05 +0200

    remove Hybrid Base

--- blog/wordpress/wp-content/themes/myoos/sidebar-primary.php
+++
@@ -1,9 +0,0 @@
-<?php if ( is_active_sidebar( 'primary' ) ) { ?>
-
-	<aside id="sidebar-primary" class="sidebar">
-
-		<?php dynamic_sidebar( 'primary' ); ?>
-
-	</aside><!-- #sidebar-primary .aside -->
-
-<?php } ?>
--- blog/wordpress/wp-content/themes/myoos/sidebar-subsidiary.php
+++
@@ -1,9 +0,0 @@
-<?php if ( is_active_sidebar( 'subsidiary' ) ) { ?>
-
-	<aside id="sidebar-subsidiary" class="sidebar">
-
-		<?php dynamic_sidebar( 'subsidiary' ); ?>
-
-	</aside><!-- #sidebar-subsidiary .aside -->
-
-<?php } ?>
