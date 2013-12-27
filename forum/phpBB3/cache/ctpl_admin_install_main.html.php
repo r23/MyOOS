50c8a4ac3db6a24190d96894e9f587fe024f6b5c
r23 <info@oos-shop.de>
2013-12-27 15:20:39 +0100

    misc update

--- forum/phpBB3/cache/ctpl_admin_install_main.html.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('IN_PHPBB')) exit; $this->_tpl_include('install_header.html'); ?>
-
-
-	<h1><?php echo (isset($this->_rootref['TITLE'])) ? $this->_rootref['TITLE'] : ''; ?></h1>
-	<p><?php echo (isset($this->_rootref['BODY'])) ? $this->_rootref['BODY'] : ''; ?></p>
-
-<?php $this->_tpl_include('install_footer.html'); ?>
