58e492280aee666d9e3ac4c8b11831dbffa7c11e
r23 <info@oos-shop.de>
2014-08-16 02:41:05 +0200

    remove

--- tracking/piwik/plugins/CoreUpdater/javascripts/updateLayout.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('#showSql').click(function () {
-        $('#sqlQueries').toggle();
-    });
-    $('#upgradeCorePluginsForm').submit(function () {
-        $('input[type=submit]', this).prop('disabled', 'disabled');
-    });
-});
