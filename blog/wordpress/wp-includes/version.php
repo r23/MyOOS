ff837bf0d0ca1c566c272b28101a754b1874b479
r23 <info@oos-shop.de>
2015-04-27 23:50:46 +0200

    update Wordpress 4.2.1

--- blog/wordpress/wp-includes/version.php
+++ blog/wordpress/wp-includes/version.php
@@ -4,14 +4,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.2';
+$wp_version = '4.2.1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 31532;
+$wp_db_version = 31533;
 
 /**
  * Holds the TinyMCE version
@@ -33,5 +33,3 @@ $required_php_version = '5.2.4';
  * @global string $required_mysql_version
  */
 $required_mysql_version = '5.0';
-
-$wp_local_package = 'de_DE';
