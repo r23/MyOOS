28310948fd36185bbd09efc8f2bb9eb95ed2c535
r23 <info@oos-shop.de>
2015-06-14 18:47:46 +0200

    import Blank-Theme Underscores

---
+++ blog/wordpress/wp-content/themes/myoos/template-parts/content-single.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template part for displaying single posts.
+ *
+ * @package myoos
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php myoos_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'myoos' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php myoos_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
