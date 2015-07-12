21122b38204d25b30211a6a013b99141f74d54cb
r23 <info@oos-shop.de>
2015-07-12 20:16:18 +0200

    import myOOS-LESS

--- blog/wordpress/wp-content/themes/myoos/footer.php
+++ blog/wordpress/wp-content/themes/myoos/footer.php
@@ -13,9 +13,9 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'myoos' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'myoos' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'myoos' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'myoos' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'myoos' ), 'myoos', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'myoos' ), 'myoos', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- blog/wordpress/wp-content/themes/myoos/template-parts/content.php
+++ blog/wordpress/wp-content/themes/myoos/template-parts/content.php
@@ -20,9 +20,9 @@
 
 	<div class="entry-content">
 		<?php
-			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'myoos' ), array( 'span' => array( 'class' => array() ) ) ),
+				/* translators: %s: Name of current post. */
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 		?>
