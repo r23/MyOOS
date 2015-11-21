14246811513f3116873e29a3dd8806eb40f81717
r23 <info@oos-shop.de>
2015-11-21 15:49:44 +0100

    Update WordPress Seo

--- blog/wordpress/wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ blog/wordpress/wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitc22fed993fc958d9c710af64e5c9df30::getLoader();
+return ComposerAutoloaderInit9db84d06c3df7b729a6b3aa5b2f81403::getLoader();
--- blog/wordpress/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ blog/wordpress/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit0c156aec84008eb9b02f76b426b164e9::getLoader();
+return ComposerAutoloaderInitb8796cbd61e1b246b6df3810270d9d1d::getLoader();
