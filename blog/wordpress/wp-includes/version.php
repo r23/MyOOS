b0b63b236f3e766cb99355665b8bf363d56441aa
r23 <info@oos-shop.de>
2016-04-13 00:56:31 +0200

    update WordPress 4.5

--- blog/wordpress/wp-includes/version.php
+++ blog/wordpress/wp-includes/version.php
@@ -4,21 +4,21 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.4.2';
+$wp_version = '4.5';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 35700;
+$wp_db_version = 36686;
 
 /**
  * Holds the TinyMCE version
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4208-20151113';
+$tinymce_version = '4308-20160323';
 
 /**
  * Holds the required PHP version
@@ -33,3 +33,5 @@ $required_php_version = '5.2.4';
  * @global string $required_mysql_version
  */
 $required_mysql_version = '5.0';
+
+$wp_local_package = 'de_DE';
