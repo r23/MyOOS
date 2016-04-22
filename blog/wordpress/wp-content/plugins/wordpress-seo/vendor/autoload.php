b3e099c3a2077929a38a4d4f5aad2b13a5b377ba
r23 <info@oos-shop.de>
2016-04-22 19:38:54 +0200

    update WordPress Plug-Ins

--- blog/wordpress/wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ blog/wordpress/wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit3b6a4908feb2b93a2f82bfcaba66716c::getLoader();
+return ComposerAutoloaderInit8f412f0bce78c2c2ae84a1163b773a57::getLoader();
--- blog/wordpress/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ blog/wordpress/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit86fc86fe5f84796137d2f17c7ad674fe::getLoader();
+return ComposerAutoloaderInitd63c710cea090f99b7d88fe03d096454::getLoader();
