7514a721e7bd28ded5cf5979b7894242f5ecc50c
r23 <info@oos-shop.de>
2017-04-20 20:59:34 +0200

    update WordPress 4.7.4

--- blog/wordpress/wp-includes/version.php
+++ blog/wordpress/wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '4.7.3';
+$wp_version = '4.7.4';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 38590;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '4403-20160901';
+$tinymce_version = '4506-20170408';
 
 /**
  * Holds the required PHP version
@@ -33,5 +33,3 @@ $required_php_version = '5.2.4';
  * @global string $required_mysql_version
  */
 $required_mysql_version = '5.0';
-
-$wp_local_package = 'de_DE';
