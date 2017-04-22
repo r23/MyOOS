e3445e8c603abe76af5fa3bb05e90efa278521e0
r23 <info@oos-shop.de>
2017-04-22 20:58:08 +0200

    import 	Plugin Name: R23 Sketchfab Plugin Description: Display Sketchfab models to wordpress.

---
+++ blog/wordpress/wp-content/plugins/r23-sketchfab/admin/js/editor_plugin.js
@@ -0,0 +1,22 @@
+(function() {
+	tinymce.create('tinymce.plugins.sketchfab', {
+		
+		init : function(ed, url){
+			ed.addButton('sketchfab', {
+				title : 'X3DOM 3D models Viewers',
+				onclick : function() {
+					var ed = tinyMCE.activeEditor;
+					ed.focus();
+					var sel = ed.selection;
+					var content = sel.getContent();
+					content='[sketchfab id="2996aa43a7794a889ea8bbbe62d9140e"]';
+					sel.setContent(content);
+				},
+				image: url + "/icon_shortcodes.png"
+			}	
+		);
+	},
+});
+
+tinymce.PluginManager.add('sketchfab', tinymce.plugins.sketchfab);
+})();
