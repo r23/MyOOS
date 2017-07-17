0afd615cc502d1c2dd02cd6322082e7369766d88
r23 <info@oos-shop.de>
2017-07-17 22:26:23 +0200

    update products_history

--- shop/includes/blocks/block_products_history.php
+++ shop/includes/blocks/block_products_history.php
@@ -9,20 +9,20 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------- */
 
-  /** ensure this file is being included by a parent file */
-  defined( 'OOS_VALID_MOD' ) OR die( 'Direct Access to this location is not allowed.' );
+/** ensure this file is being included by a parent file */
+defined( 'OOS_VALID_MOD' ) OR die( 'Direct Access to this location is not allowed.' );
 
-  $products_history_block = 'false';
+$products_history_block = FALSE;
 
 if (isset($_SESSION)) {
   
-  if ($_SESSION['products_history']->count_history() > 0) {
-    $products_history_block = 'true';
-    $product_ids = $_SESSION['products_history']->get_product_id_list();
+	if ($_SESSION['products_history']->count_history() > 0) {
+		$products_history_block = TRUE;
+		$product_ids = $_SESSION['products_history']->get_product_id_list();
 
-    $productstable = $oostable['products'];
-    $products_descriptiontable = $oostable['products_description'];
-    $products_sql = "SELECT p.products_id, p.products_image, pd.products_name,
+		$productstable = $oostable['products'];
+		$products_descriptiontable = $oostable['products_description'];
+		$products_sql = "SELECT p.products_id, p.products_image, pd.products_name,
                             substring(pd.products_description, 1, 150) AS products_description
                      FROM $productstable p,
                           $products_descriptiontable pd
@@ -30,11 +30,11 @@ if (isset($_SESSION)) {
                        AND p.products_id = pd.products_id
                        AND pd.products_languages_id = '" .  intval($nLanguageID) . "'
                      ORDER BY products_name";
-    $smarty->assign('customer_products_history', $dbconn->GetAll($products_sql));
-    $smarty->assign('block_heading_products_history', $block_heading);
-  }
+		$smarty->assign('customer_products_history', $dbconn->GetAll($products_sql));
+		$smarty->assign('block_heading_products_history', $block_heading);
+	}
 }
 
-  $smarty->assign('products_history_block', $products_history_block);
+$smarty->assign('products_history_block', $products_history_block);
 
 
