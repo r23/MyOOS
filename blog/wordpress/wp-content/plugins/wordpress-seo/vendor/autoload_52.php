345793843bd880148c0a73b54e8b8c161a890828
r23 <info@oos-shop.de>
2017-10-10 17:53:56 +0200

    update WordPress SEO

--- blog/wordpress/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ blog/wordpress/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit1b4dbfdb4747cd440543af13b3cc854f::getLoader();
+return ComposerAutoloaderInit2da3cc58521ce09b8a6716ef9756e74c::getLoader();
