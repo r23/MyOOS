a2bd66098555171b5805dd2bd8ad1ba2dadbe462
r23 <info@oos-shop.de>
2017-12-24 12:14:07 +0100

    import translation for excel import

---
+++ shop/admin/includes/languages/eng/import_excel.php
@@ -0,0 +1,29 @@
+<?php
+/* ----------------------------------------------------------------------
+   $Id: export_excel.php,v 1.3 2007/06/13 16:38:21 r23 Exp $
+
+   MyOOS [Shopsystem]
+   https://www.oos-shop.de
+
+   Copyright (c) 2003 - 2017 by the MyOOS Development Team.
+   ----------------------------------------------------------------------
+   Based on:
+
+   File: backup.php,v 1.16 2002/03/16 21:30:02 hpdl
+   ----------------------------------------------------------------------
+   osCommerce, Open Source E-Commerce Solutions
+   http://www.oscommerce.com
+
+   Copyright (c) 2003 osCommerce
+   ----------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   If you made a translation, please send to
+      lang@oos-shop.de
+   the translated file.
+   ---------------------------------------------------------------------- */
+
+
+define('HEADING_TITLE', 'Update Excel Price');
