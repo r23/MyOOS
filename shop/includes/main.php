7ce611f7744f06a09393b4a0efdb4896d5cc8e77
r23 <info@oos-shop.de>
2017-12-29 16:36:38 +0100

    welcome 2.108-dev

--- shop/includes/main.php
+++ shop/includes/main.php
@@ -34,7 +34,7 @@ if (is_readable('includes/local/configure.php')) {
 }
 
 // Version information
-define('OOS_VERSION', '2.0.107 -dev');
+define('OOS_VERSION', '2.0.108 -dev');
 // Complete software name string
 define('OOS_FULL_NAME', 'MyOOS ' . OOS_VERSION);
 
