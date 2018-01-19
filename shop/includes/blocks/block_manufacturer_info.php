c2b0fd8ae602202f1ce3498705732ad8ae0c3435
r23 <info@oos-shop.de>
2018-01-19 19:24:26 +0100

    update manufacturer_info for Bootstrap 4

--- shop/includes/blocks/block_manufacturer_info.php
+++ shop/includes/blocks/block_manufacturer_info.php
@@ -18,18 +18,18 @@
    Released under the GNU General Public License
    ---------------------------------------------------------------------- */
 
-  /** ensure this file is being included by a parent file */
-  defined( 'OOS_VALID_MOD' ) OR die( 'Direct Access to this location is not allowed.' );
+/** ensure this file is being included by a parent file */
+defined( 'OOS_VALID_MOD' ) OR die( 'Direct Access to this location is not allowed.' );
 
-  if (!$oEvent->installed_plugin('manufacturers')) return FALSE;
+if (!$oEvent->installed_plugin('manufacturers')) return FALSE;
 
-  $manufacturer_info_block = FALSE;
+$manufacturer_info_block = FALSE;
 
-  if (isset($_GET['products_id'])) {
-    if (!isset($nProductsID)) $nProductsID = oos_get_product_id($_GET['products_id']);
+if (isset($_GET['products_id'])) {
+	if (!isset($nProductsID)) $nProductsID = oos_get_product_id($_GET['products_id']);
 
     $manufacturerstable = $oostable['manufacturers'];
-    $manufacturers_infotable = $oostable['manufacturers_info'];
+	$manufacturers_infotable = $oostable['manufacturers_info'];
     $productstable = $oostable['products'];
     $query = "SELECT m.manufacturers_id, m.manufacturers_name, m.manufacturers_image, mi.manufacturers_url 
               FROM $manufacturerstable m LEFT JOIN
@@ -39,21 +39,19 @@
                    $productstable p
               WHERE p.products_id = '" . intval($nProductsID) . "'
                 AND p.manufacturers_id = m.manufacturers_id";
-    $manufacturer_result = $dbconn->Execute($query);
+	$manufacturer_result = $dbconn->Execute($query);
 
-    if ($manufacturer_result->RecordCount()) {
+	if ($manufacturer_result->RecordCount()) {
 
-      $manufacturer = $manufacturer_result->fields;
-      $manufacturer_info_block = TRUE;
-
-      $smarty->assign(
+		$manufacturer = $manufacturer_result->fields;
+		$manufacturer_info_block = TRUE;
+		$smarty->assign(
           array(
               'manufacturer' => $manufacturer,
               'block_heading_manufacturer_info' => $block_heading
-          )
-      );
+			)
+		);
     }
-  }
-  $smarty->assign('manufacturer_info_block', $manufacturer_info_block);
+}
 
-
+$smarty->assign('manufacturer_info_block', $manufacturer_info_block);
