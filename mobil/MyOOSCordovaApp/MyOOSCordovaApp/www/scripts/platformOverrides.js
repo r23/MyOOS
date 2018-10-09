677fdf85d7eba5f9e0582718f9fcca6eeb41fa56
r23 <info@oos-shop.de>
2018-10-09 16:58:56 +0200

    remove Apache Cordova

--- mobil/MyOOSCordovaApp/MyOOSCordovaApp/www/scripts/platformOverrides.js
+++
@@ -1,4 +0,0 @@
-﻿/*
-	Diese Datei wird durch plattformspezifischen Code aus dem Ordner "/merges" ersetzt.
-	Weitere Informationen finden Sie unter "http://taco.visualstudio.com/de-de/docs/configure-app/#Content".
-*/
