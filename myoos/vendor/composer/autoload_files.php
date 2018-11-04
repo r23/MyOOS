fbd893436d857f314258700ab669735f47ec026f
r23 <info@oos-shop.de>
2018-11-04 15:02:00 +0100

    - Updating symfony/polyfill-apcu (v1.9.0 => v1.10.0)   - Updating symfony/polyfill-ctype (v1.9.0 => v1.10.0)   - Updating symfony/polyfill-mbstring (v1.9.0 => v1.10.0)   - Updating symfony/polyfill-php70 (v1.9.0 => v1.10.0)   - Updating symfony/polyfill-util (v1.9.0 => v1.10.0)   - Updating symfony/polyfill-php56 (v1.9.0 => v1.10.0)   - Updating symfony/symfony (v3.4.17 => v3.4.18)   - Updating symfony/polyfill-intl-icu (v1.9.0 => v1.10.0)

--- myoos/vendor/composer/autoload_files.php
+++ myoos/vendor/composer/autoload_files.php
@@ -6,8 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
     '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
+    '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
     '32dcc8afd4335739640db7d200c1971d' => $vendorDir . '/symfony/polyfill-apcu/bootstrap.php',
     '023d27dca8066ef29e6739335ea73bad' => $vendorDir . '/symfony/polyfill-php70/bootstrap.php',
     'bd9634f2d41831496de0d3dfe4c94881' => $vendorDir . '/symfony/polyfill-php56/bootstrap.php',
--- myoos/vendor/composer/autoload_static.php
+++ myoos/vendor/composer/autoload_static.php
@@ -7,8 +7,8 @@ namespace Composer\Autoload;
 class ComposerStaticInit4d6b45e6a70704e8709ae49fdd543789
 {
     public static $files = array (
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '32dcc8afd4335739640db7d200c1971d' => __DIR__ . '/..' . '/symfony/polyfill-apcu/bootstrap.php',
         '023d27dca8066ef29e6739335ea73bad' => __DIR__ . '/..' . '/symfony/polyfill-php70/bootstrap.php',
         'bd9634f2d41831496de0d3dfe4c94881' => __DIR__ . '/..' . '/symfony/polyfill-php56/bootstrap.php',
