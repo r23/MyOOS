d83044fa7556af45cff06dc362dd50636a56f801
r23 <info@oos-shop.de>
2019-03-18 17:28:07 +0100

    remove breadcrumbs from nav_menu

--- myoos/includes/classes/class_nav_menu.php
+++ myoos/includes/classes/class_nav_menu.php
@@ -29,15 +29,13 @@ class nav_menu {
         $root_end_string = '',
         $parent_start_string = '',
         $parent_end_string = '',
-        $parent_group_start_string = '',
-        $parent_group_end_string = '',
-        $child_start_string = '<li class="main-nav-item main-nav-expanded">',
-        $child_end_string = '</li>',
-        $breadcrumb_separator = '_',
-        $breadcrumb_usage = TRUE,
+        $parent_group_start_string = '<li class="main-nav-item main-nav-expanded">',
+        $parent_group_end_string = '</li>',
+        $child_start_string = '',
+        $child_end_string = '',
         $spacer_string = '',
         $spacer_multiplier = 1,
-        $follow_cpath = FALSE,
+        $follow_cpath = TRUE,
         $cpath_array = array(),
         $cpath_start_string = '',
         $cpath_end_string = '';
@@ -74,16 +72,6 @@ class nav_menu {
 		}
     }
 
-    public function setData(&$data_array) {
-		if (is_array($data_array)) {
-			$this->data = array();
-
-			for ($i=0, $n=count($data_array); $i<$n; $i++) {
-				$this->data[$data_array[$i]['parent_id']][$data_array[$i]['categories_id']] = array('name' => $data_array[$i]['categories_name'], 'count' => $data_array[$i]['categories_count']);
-			}
-		}
-    }
-
     public function buildBranch($parent_id, $level = 0) {
 		$result = $this->parent_group_start_string;
 
@@ -91,13 +79,7 @@ class nav_menu {
 
 		if (isset($this->data[$parent_id])) {
 			foreach ($this->data[$parent_id] as $category_id => $category) {
-				if ($this->breadcrumb_usage == TRUE) {
-					$category_link = $this->buildBreadcrumb($category_id);
-				} else {
-					$category_link = $category_id;
-				}
-
-				$sLink = '<a href="' . oos_href_link($aContents['shop'], 'category=' . $category_link) . '" title="' . $category['name'] . '">';
+				$sLink = '<a href="' . oos_href_link($aContents['shop'], 'category=' . $category_id) . '" title="' . $category['name'] . '">';
 
 				$result .= $this->child_start_string;
 
@@ -105,7 +87,7 @@ class nav_menu {
 					$result .= $this->parent_start_string;
 				}
 
-				if ($level == 0) {
+				if ( $level === 0 ) {
 					$result .= $this->root_start_string;
 				}
 
@@ -113,7 +95,7 @@ class nav_menu {
 
 				$result .= $sLink;
 				
-				if ($level == 0) {
+				if ( $level === 0 ) {
 					// todo add color
 					$result .= '<i class="fa fa-circle-o-notch" aria-hidden="true"></i>';
 				}
@@ -130,7 +112,7 @@ class nav_menu {
 				$result .= '</a>';
 
 
-				if ($level == 0) {
+				if ( $level === 0 ) {
 					$result .= $this->root_end_string;
 				}
 
@@ -158,85 +140,59 @@ class nav_menu {
 	}
 
 
-    public function buildBranchArray($parent_id, $level = 0, $result = '') {
-      if (empty($result)) {
-        $result = array();
-      }
+	public function buildBranchArray($parent_id, $level = 0, $result = '') {
+		if (empty($result)) {
+			$result = array();
+		}
 
-      if (isset($this->data[$parent_id])) {
-        foreach ($this->data[$parent_id] as $category_id => $category) {
-          if ($this->breadcrumb_usage == TRUE) {
-            $category_link = $this->buildBreadcrumb($category_id);
-          } else {
-            $category_link = $category_id;
-          }
+		if (isset($this->data[$parent_id])) {
+			foreach ($this->data[$parent_id] as $category_id => $category) {
+				$category_link = $category_id;
 
-          $result[] = array('id' => $category_link,
-                            'title' => str_repeat($this->spacer_string, $this->spacer_multiplier * $level) . $category['name']);
+				$result[] = array('id' => $category_link,
+									'title' => str_repeat($this->spacer_string, $this->spacer_multiplier * $level) . $category['name']);
 
-          if (isset($this->data[$category_id]) && (($this->max_level == '0') || ($this->max_level > $level+1))) {
-            if ($this->follow_cpath === TRUE) {
-              if (in_array($category_id, $this->cpath_array)) {
-                $result = $this->buildBranchArray($category_id, $level+1, $result);
-              }
-            } else {
-              $result = $this->buildBranchArray($category_id, $level+1, $result);
-            }
-          }
-        }
-      }
+				if (isset($this->data[$category_id]) && (($this->max_level == '0') || ($this->max_level > $level+1))) {
+					if ($this->follow_cpath === TRUE) {
+						if (in_array($category_id, $this->cpath_array)) {
+							$result = $this->buildBranchArray($category_id, $level+1, $result);
+						}
+					} else {
+						$result = $this->buildBranchArray($category_id, $level+1, $result);
+					}
+				}
+			}
+		}
 
-      return $result;
+		return $result;
     }
 
-
-    public function buildBreadcrumb($category_id, $level = 0) {
-      $breadcrumb = '';
-
-      foreach ($this->data as $parent => $categories) {
-        foreach ($categories as $id => $info) {
-          if ($id == $category_id) {
-            if ($level < 1) {
-              $breadcrumb = $id;
-            } else {
-              $breadcrumb = $id . $this->breadcrumb_separator . $breadcrumb;
-            }
-
-            if ($parent != $this->root_category_id) {
-              $breadcrumb = $this->buildBreadcrumb($parent, $level+1) . $breadcrumb;
-            }
-          }
-        }
-      }
-
-      return $breadcrumb;
-    }
 
     public function build() {
 		return $this->buildBranch($this->root_category_id);
     }
 
     public function getTree($parent_id = '') {
-      return $this->buildBranchArray((empty($parent_id) ? $this->root_category_id : $parent_id));
+		return $this->buildBranchArray((empty($parent_id) ? $this->root_category_id : $parent_id));
     }
 
 
     public function setRootCategoryID($root_category_id) {
-      $this->root_category_id = $root_category_id;
+		$this->root_category_id = $root_category_id;
     }
 
     public function setMaximumLevel($max_level) {
-      $this->max_level = $max_level;
+		$this->max_level = $max_level;
     }
 
     public function setRootString($root_start_string, $root_end_string) {
-      $this->root_start_string = $root_start_string;
-      $this->root_end_string = $root_end_string;
+		$this->root_start_string = $root_start_string;
+		$this->root_end_string = $root_end_string;
     }
 
     public function setParentString($parent_start_string, $parent_end_string) {
-      $this->parent_start_string = $parent_start_string;
-      $this->parent_end_string = $parent_end_string;
+		$this->parent_start_string = $parent_start_string;
+		$this->parent_end_string = $parent_end_string;
     }
 
     public function setParentGroupString($parent_group_start_string, $parent_group_end_string) {
@@ -245,58 +201,45 @@ class nav_menu {
     }
 
     public function setChildString($child_start_string, $child_end_string) {
-      $this->child_start_string = $child_start_string;
-      $this->child_end_string = $child_end_string;
-    }
-
-    public function setBreadcrumbSeparator($breadcrumb_separator) {
-      $this->breadcrumb_separator = $breadcrumb_separator;
-    }
-
-    public function setBreadcrumbUsage($breadcrumb_usage) {
-      if ($breadcrumb_usage === TRUE) {
-        $this->breadcrumb_usage = TRUE;
-      } else {
-        $this->breadcrumb_usage = FALSE;
-      }
+		$this->child_start_string = $child_start_string;
+		$this->child_end_string = $child_end_string;
     }
 
     public function setSpacerString($spacer_string, $spacer_multiplier = 2) {
-      $this->spacer_string = $spacer_string;
-      $this->spacer_multiplier = $spacer_multiplier;
+		$this->spacer_string = $spacer_string;
+		$this->spacer_multiplier = $spacer_multiplier;
     }
 
     public function setCategoryPath($cpath, $cpath_start_string = '', $cpath_end_string = '') {
-      $this->follow_cpath = TRUE;
-      $this->cpath_array = explode($this->breadcrumb_separator, $cpath);
-      $this->cpath_start_string = $cpath_start_string;
-      $this->cpath_end_string = $cpath_end_string;
+		$this->follow_cpath = TRUE;
+		$this->cpath_start_string = $cpath_start_string;
+		$this->cpath_end_string = $cpath_end_string;
     }
 
     public function setFollowCategoryPath($follow_cpath) {
-      if ($follow_cpath === TRUE) {
-        $this->follow_cpath = TRUE;
-      } else {
-        $this->follow_cpath = FALSE;
-      }
+		if ($follow_cpath === TRUE) {
+			$this->follow_cpath = TRUE;
+		} else {
+			$this->follow_cpath = FALSE;
+		}
     }
 
-    public function setCategoryPathString($cpath_start_string, $cpath_end_string) {
-      $this->cpath_start_string = $cpath_start_string;
-      $this->cpath_end_string = $cpath_end_string;
+	public function setCategoryPathString($cpath_start_string, $cpath_end_string) {
+		$this->cpath_start_string = $cpath_start_string;
+		$this->cpath_end_string = $cpath_end_string;
     }
 
-    public function setShowCategoryProductCount($show_category_product_count) {
-      if ($show_category_product_count === TRUE) {
-        $this->show_category_product_count = TRUE;
-      } else {
-        $this->show_category_product_count = FALSE;
-      }
+	public function setShowCategoryProductCount($show_category_product_count) {
+		if ($show_category_product_count === TRUE) {
+			$this->show_category_product_count = TRUE;
+		} else {
+			$this->show_category_product_count = FALSE;
+		}
     }
 
-    public function setCategoryProductCountString($category_product_count_start_string, $category_product_count_end_string) {
-      $this->category_product_count_start_string = $category_product_count_start_string;
-      $this->category_product_count_end_string = $category_product_count_end_string;
-    }
-  }
+	public function setCategoryProductCountString($category_product_count_start_string, $category_product_count_end_string) {
+		$this->category_product_count_start_string = $category_product_count_start_string;
+		$this->category_product_count_end_string = $category_product_count_end_string;
+	}
+}
 
