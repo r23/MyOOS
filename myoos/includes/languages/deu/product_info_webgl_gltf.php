232a34e56646177732ad11348bee14f068d402ff
r23 <info@oos-shop.de>
2019-07-30 02:57:18 +0200

    import product_info_webgl_gltf

---
+++ myoos/includes/languages/deu/product_info_webgl_gltf.php
@@ -0,0 +1,54 @@
+<?php
+/* ----------------------------------------------------------------------
+   $Id: products_info.php,v 1.3 2007/06/12 16:36:39 r23 Exp $
+
+   MyOOS [Shopsystem]
+   https://www.oos-shop.de
+
+   Copyright (c) 2003 - 2019 by the MyOOS Development Team.
+   ----------------------------------------------------------------------
+   Based on:
+
+   File: product_info.php,v 1.18 2003/02/16 00:42:03 harley_vb
+   ----------------------------------------------------------------------
+   osCommerce, Open Source E-Commerce Solutions
+   http://www.oscommerce.com
+
+   Copyright (c) 2003 osCommerce
+   ----------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------- */
+
+$aLang['text_product_not_found'] = 'Produkt wurde nicht gefunden!';
+$aLang['text_current_reviews'] = 'Aktuelle Meinungen:';
+$aLang['text_more_information'] = 'Für weitere Informationen, besuchen Sie bitte die <a href="%s" target="_blank" rel="noopener"><u>Homepage</u></a> zu diesem Produkt.';
+$aLang['text_manufacturer_info_homepage'] = '%s Homepage';
+$aLang['text_date_added'] = 'Dieses Produkt haben wir am %s in unseren Katalog aufgenommen.';
+$aLang['text_date_available'] = 'Dieses Produkt wird voraussichtlich ab %s wieder vorrätig sein.';
+$aLang['text_also_purchased_products'] = 'Zusätzlich kauften Kunden auch ';
+$aLang['text_xsell_products'] = 'Ähnliche Produkte:';
+$aLang['text_up_sell_products'] = 'Zu diesem Artikel empfehlen wir:';
+$aLang['text_optional_accessories'] = 'Optionales Zubehör';
+$aLang['text_product_options'] = 'Optionen:';
+$aLang['text_click_to_enlarge'] = 'Für eine grössere Darstellung<br />klicken Sie auf das Bild.';
+$aLang['text_close_window'] = 'Fenster schließen [x]';
+$aLang['products_order_qty_text'] = 'Menge';
+$aLang['text_product_notifications'] = 'Ihre Produkt Benachrichtigungen';
+$aLang['text_product_notifications_introduction'] = 'Mit der Benachrichtigungsfunktion ermöglichen wir es Ihnen, über alle unsere Produkte informiert zu sein. Sie erhalten zeitnah die aktuellsten Informationen über unsere Produkte und Sonderangebote.';
+
+$aLang['table_heading_image'] = '';
+$aLang['table_heading_model'] = 'Artikel-Nr.';
+$aLang['table_heading_products'] = 'Produkte';
+$aLang['table_heading_manufacturer'] = 'Hersteller';
+$aLang['table_heading_quantity'] = 'Anzahl';
+$aLang['table_heading_list_price'] = 'UVP';
+$aLang['table_heading_price'] = 'Preis';
+$aLang['table_heading_weight'] = 'Gewicht';
+$aLang['table_heading_buy_now'] = 'Bestellen';
+
+// modules/slavery_listing.php
+$aLang['text_buy'] = '1 x \'';
+$aLang['text_now'] = '\' bestellen!';
+
+$aLang['navigation_help'] = '<h2>Navigationshilfe</h2><p>3D-Modell drehen: Drücken der linken Maustaste und bewegen der Maus<br>Vergrößern/Verkleinern: Mausrad drehen<br>Verschieben: Dr&uuml;cken der rechten Maustaste und bewegen der Maus</p>';
+
---
+++ myoos/includes/languages/eng/product_info_webgl_gltf.php
@@ -0,0 +1,54 @@
+<?php
+/* ----------------------------------------------------------------------
+   $Id: products_info.php,v 1.3 2007/06/12 16:51:19 r23 Exp $
+
+   MyOOS [Shopsystem]
+   https://www.oos-shop.de
+
+   Copyright (c) 2003 - 2019 by the MyOOS Development Team.
+   ----------------------------------------------------------------------
+   Based on:
+
+   File: product_info.php,v 1.15 2002/11/19 01:48:08 dgw_
+   ----------------------------------------------------------------------
+   osCommerce, Open Source E-Commerce Solutions
+   http://www.oscommerce.com
+
+   Copyright (c) 2003 osCommerce
+   ----------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------- */
+
+$aLang['text_product_not_found'] = 'Product not found!';
+$aLang['text_current_reviews'] = 'Current Reviews:';
+$aLang['text_more_information'] = 'For more information, please visit this products <a href="%s" target="_blank" rel="noopener"><u>webpage</u></a>.';
+$aLang['text_manufacturer_info_homepage'] = '%s Homepage';
+$aLang['text_date_added'] = 'This product was added to our catalog on %s.';
+$aLang['text_date_available'] = 'This product will be in stock on %s.';
+$aLang['text_also_purchased_products'] = 'Customers who bought this product also purchased';
+$aLang['text_xsell_products'] = 'Family Products:';
+$aLang['text_up_sell_products'] = 'Best Value:';
+$aLang['text_product_options'] = 'Available Options:';
+$aLang['text_optional_accessories'] = 'Optional accessories:';
+$aLang['text_click_to_enlarge'] = 'Click to enlarge';
+$aLang['text_close_window'] = 'Close Window [x]';
+$aLang['products_order_qty_text'] = 'Qty.';
+$aLang['text_product_notifications'] = 'My Product Notifications';
+$aLang['text_product_notifications_introduction'] = 'The product notification list allows you to stay up to date on the products we have available, which gives you the chance to take advantage of our products on special.';
+
+$aLang['table_heading_image'] = '';
+$aLang['table_heading_model'] = 'Model';
+$aLang['table_heading_products'] = 'Product Name';
+$aLang['table_heading_manufacturer'] = 'Manufacturer';
+$aLang['table_heading_quantity'] = 'Quantity';
+$aLang['table_heading_list_price'] = 'List';
+$aLang['table_heading_price'] = 'Price';
+$aLang['table_heading_weight'] = 'Weight';
+$aLang['table_heading_buy_now'] = 'Buy Now';
+
+// modules/slavery_listing.php
+$aLang['text_buy'] = 'Buy 1 \'';
+$aLang['text_now'] = '\' now';
+
+$aLang['navigation_help'] = '<h2>Navigation Help</h2><p>Rotate 3D model: Press the left mouse button and move the mouse.<br>Zoom in/out: Rotate mouse wheel<br>Move: Press the right mouse button and move the mouse.</p>';
+
