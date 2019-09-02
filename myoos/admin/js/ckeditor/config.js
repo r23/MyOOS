d1a82552bbbf37ecf0fba1824c17b5613ac4dd8a
r23 <info@oos-shop.de>
2019-09-02 19:41:44 +0200

    update ckeditor_4.12.1

--- myoos/admin/js/ckeditor/config.js
+++ myoos/admin/js/ckeditor/config.js
@@ -35,8 +35,4 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
-	
-	config.allowedContent = 'u em strong ul li;div[!id]';
-
-	
 };
