e0b8c63ccbb204998cfd97f62ae8eddfcf675155
r23 <info@oos-shop.de>
2019-09-14 17:31:06 +0200

    welcome 2.3.9

--- myoos/install/step.php
+++ myoos/install/step.php
@@ -73,7 +73,7 @@ if (version_compare(PHP_VERSION, '7.2.0', '<')) {
 
 define('OOS_VALID_MOD', true);
 // Version information
-define('OOS_VERSION', '2.3.8 -dev');
+define('OOS_VERSION', '2.3.9 -dev');
 
 require_once '../includes/functions/function_global.php';
 require_once '../includes/functions/function_kernel.php';
