135407cc7a8fc4b96bc4f723af95578bfeef9ba2
r23 <info@oos-shop.de>
2020-04-05 15:49:30 +0200

    welcome 2.4.4 -dev

--- myoos/install/step.php
+++ myoos/install/step.php
@@ -73,7 +73,7 @@ if (version_compare(PHP_VERSION, '7.2.0', '<')) {
 
 define('OOS_VALID_MOD', true);
 // Version information
-define('OOS_VERSION', 'MyOOS 2.4.3 -dev');
+define('OOS_VERSION', 'MyOOS 2.4.4 -dev');
 
 require_once '../includes/functions/function_global.php';
 require_once '../includes/functions/function_kernel.php';
