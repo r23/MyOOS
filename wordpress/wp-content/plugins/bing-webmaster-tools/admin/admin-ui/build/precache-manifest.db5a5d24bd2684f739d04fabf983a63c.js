8611a4ff619660b48e6edc3bf779b796a6987852
r23 <info@r23.de>
2020-08-06 18:27:09 +0200

    update Bing Webmaster Url Submission

---
+++ wordpress/wp-content/plugins/bing-webmaster-tools/admin/admin-ui/build/precache-manifest.db5a5d24bd2684f739d04fabf983a63c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4d32b1198d56987a5fec4d94a04308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a8f5e5a50af87a7bcf",
+    "url": "/static/css/main.e08700b4.chunk.css"
+  },
+  {
+    "revision": "75c3f7422ed1e9ce5958",
+    "url": "/static/js/2.c927f4a5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.c927f4a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a8f5e5a50af87a7bcf",
+    "url": "/static/js/main.9908cf66.chunk.js"
+  },
+  {
+    "revision": "a64f0e4cd7a4d3044703",
+    "url": "/static/js/runtime-main.eaca026f.js"
+  }
+]);
