59f9ce327650d49e900c38ba774f88ad82bc15c3
r23 <info@r23.de>
2021-01-14 02:21:13 +0100

    remove old theme

--- wordpress/wp-content/themes/myoos/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-= 0.9.0.1 =
-* Fixed update checker url
-* Updated POT file
-
-= 0.9 =
-* Initial release
--- wordpress/wp-content/themes/myoos/template-parts/calendar-plus/taxonomy-calendar_event_category.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
-calendarp_get_template( 'archive-event.php' );
