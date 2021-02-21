fde0c692e306b5d9d32008225127ce83d3b2d70b
r23 <info@r23.de>
2021-02-21 16:04:54 +0100

    Warning: Trying to access array offset on value of type bool in ~/admin/includes/functions/function_informations.php on line 31 Warning: Trying to access array offset on value of type bool in ~/admin/includes/functions/function_informations.php on line 75 Warning: Trying to access array offset on value of type bool in ~/admin/includes/functions/function_informations.php on line 54

--- myoos/admin/includes/functions/function_informations.php
+++ myoos/admin/includes/functions/function_informations.php
@@ -28,7 +28,7 @@ function oos_get_informations_name($informations_id, $language_id = '') {
                 AND information_languages_id = '" . intval($language_id) . "'";
     $result = $dbconn->Execute($query);
 
-    $information_name = $result->fields['information_name'];
+    $information_name = isset($result->fields['information_name']) ? $result->fields['information_name'] : '';
 
 
     return $information_name;
@@ -51,7 +51,7 @@ function oos_get_informations_description($informations_id, $language_id = '') {
                 AND information_languages_id = '" . intval($language_id) . "'";
     $result = $dbconn->Execute($query);
 
-    $information_description = $result->fields['information_description'];
+    $information_description = isset($result->fields['information_description']) ? $result->fields['information_description'] : '';
 
     return $information_description;
 }
@@ -72,7 +72,7 @@ function oos_get_informations_heading_title($informations_id, $language_id = '')
                 AND information_languages_id = '" . intval($language_id) . "'";
     $result = $dbconn->Execute($query);
 
-    $information_heading_title = $result->fields['information_heading_title'];
+    $information_heading_title = isset($result->fields['information_heading_title']) ? $result->fields['information_heading_title'] : '';
 
     return  $information_heading_title;
 }
