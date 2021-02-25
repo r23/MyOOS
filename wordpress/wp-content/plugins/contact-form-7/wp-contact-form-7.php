05b975a6ecfa1a77f5508896fc37b1d77d5d8d67
r23 <info@r23.de>
2021-02-25 02:12:06 +0100

    update WordPress Contact Form 7

--- wordpress/wp-content/plugins/contact-form-7/wp-contact-form-7.php
+++ wordpress/wp-content/plugins/contact-form-7/wp-contact-form-7.php
@@ -7,12 +7,12 @@ Author: Takayuki Miyoshi
 Author URI: https://ideasilo.wordpress.com/
 Text Domain: contact-form-7
 Domain Path: /languages/
-Version: 5.3.2
+Version: 5.4
 */
 
-define( 'WPCF7_VERSION', '5.3.2' );
+define( 'WPCF7_VERSION', '5.4' );
 
-define( 'WPCF7_REQUIRED_WP_VERSION', '5.4' );
+define( 'WPCF7_REQUIRED_WP_VERSION', '5.5' );
 
 define( 'WPCF7_TEXT_DOMAIN', 'contact-form-7' );
 
