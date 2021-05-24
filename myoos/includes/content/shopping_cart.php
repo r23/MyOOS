1b2725cb776e6a47bbb7442eabe0792837669000
r23 <info@r23.de>
2021-05-24 16:14:06 +0200

    add shipping - not ready yet

--- myoos/includes/content/shopping_cart.php
+++ myoos/includes/content/shopping_cart.php
@@ -28,10 +28,150 @@ $any_out_of_stock = 0;
 
 
 if (isset($_SESSION)) { 
- 	
+
 	if (is_object($_SESSION['cart'])) {
 		if ($_SESSION['cart']->count_contents() > 0) {
+			
+			// Add Shipping Cost
+			require_once MYOOS_INCLUDE_PATH . '/includes/languages/' . $sLanguage . '/checkout_shipping.php';
+			require_once MYOOS_INCLUDE_PATH . '/includes/functions/function_address.php';
+ 
+			// if no shipping destination address was selected, use the customers own address as default
+			if (!isset($_SESSION['sendto'])) {
+				$_SESSION['sendto'] = $_SESSION['customer_default_address_id'];
+			} else {
+				// verify the selected shipping address
+				$address_booktable = $oostable['address_book'];
+				$sql = "SELECT COUNT(*) AS total
+						FROM $address_booktable
+						WHERE customers_id = '" . intval($_SESSION['customer_id']) . "'
+						AND address_book_id = '" . intval($_SESSION['sendto']) . "'";
+				$check_address_result = $dbconn->Execute($sql);
+				$check_address = $check_address_result->fields;
 
+				if ($check_address['total'] != '1') {
+					$_SESSION['sendto'] = $_SESSION['customer_default_address_id'];
+				}
+			}
+
+require_once MYOOS_INCLUDE_PATH . '/includes/classes/class_order.php';
+$oOrder = new order;
+
+// register a random ID in the session to check throughout the checkout procedure
+// against alterations in the shopping cart contents
+$_SESSION['cartID'] = $_SESSION['cart']->cartID;
+
+
+// if the order contains only virtual products, forward the customer to the billing page as
+// a shipping address is not needed
+if (($oOrder->content_type == 'virtual') || ($_SESSION['cart']->show_total() == 0) ) {
+	$_SESSION['shipping'] = false;
+	$_SESSION['sendto'] = false;
+	oos_redirect(oos_href_link($aContents['checkout_payment']));
+}
+
+$total_weight = $_SESSION['cart']->show_weight();
+$total_count = $_SESSION['cart']->count_contents();
+
+// load all enabled shipping modules
+require_once MYOOS_INCLUDE_PATH . '/includes/classes/class_shipping.php';
+$shipping_modules = new shipping;
+
+if ( defined('MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING') && (MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING == 'true') ) {
+	switch (MODULE_ORDER_TOTAL_SHIPPING_DESTINATION) {
+      case 'national':
+        if ($oOrder->delivery['country_id'] == STORE_COUNTRY) $pass = true; break;
+
+      case 'international':
+        if ($oOrder->delivery['country_id'] != STORE_COUNTRY) $pass = true; break;
+
+      case 'both':
+        $pass = true; break;
+
+      default:
+        $pass = false; break;
+	}
+
+	$free_shipping = false;
+	if ( ($pass == true) && ($oOrder->info['subtotal'] >= MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING_OVER) ) {
+		$free_shipping = true;
+
+		require_once MYOOS_INCLUDE_PATH . '/includes/languages/' . $sLanguage . '/modules/order_total/ot_shipping.php';
+	}
+} else {
+	$free_shipping = false;
+}
+
+
+
+// process the selected shipping method
+if ( isset($_POST['action']) && ($_POST['action'] == 'process') && 
+	( isset($_SESSION['formid']) && ($_SESSION['formid'] == $_POST['formid'])) ){	
+	
+	
+	if ( (isset($_POST['comments'])) && (is_string($_POST['comments'])) ) {
+		require_once  MYOOS_INCLUDE_PATH . '/includes/lib/htmlpurifier/library/HTMLPurifier.auto.php';
+		$config = HTMLPurifier_Config::createDefault();
+		$purifier = new HTMLPurifier($config);
+		$_SESSION['comments'] = $purifier->purify($_POST['comments']);		
+	}
+	$_SESSION['comments'] = isset($_SESSION['comments']) ? $_SESSION['comments'] : '';
+
+	if ( (oos_count_shipping_modules() > 0) || ($free_shipping == true) ) {
+		if ( (isset($_POST['shipping'])) && (strpos($_POST['shipping'], '_')) ) {
+			$_SESSION['shipping'] = $_POST['shipping'];
+
+			list($module, $method) = explode('_', $_SESSION['shipping']);
+			if ( is_object($$module) || ($_SESSION['shipping'] == 'free_free') ) {
+				
+				if ($_SESSION['shipping'] == 'free_free') {
+					$quote[0]['methods'][0]['title'] = $aLang['free_shipping_title'];
+					$quote[0]['methods'][0]['cost'] = '0';
+				} else {
+					$quote = $shipping_modules->quote($method, $module);
+				}
+				if (isset($quote['error'])) {
+					unset($_SESSION['shipping']);
+				} else {
+					if ( (isset($quote[0]['methods'][0]['title'])) && (isset($quote[0]['methods'][0]['cost'])) ) {
+						$sWay = ''; 
+						if (!empty($quote[0]['methods'][0]['title'])) {
+							$sWay = ' (' . $quote[0]['methods'][0]['title'] . ')'; 
+						}						
+						$_SESSION['shipping'] = array('id' => $_SESSION['shipping'],
+											'title' => (($free_shipping == true) ?  $quote[0]['methods'][0]['title'] : $quote[0]['module'] . $sWay), 
+                                            'cost' => $quote[0]['methods'][0]['cost']);
+
+						oos_redirect(oos_href_link($aContents['checkout_payment']));
+					}
+				}
+			} else {
+				unset($_SESSION['shipping']);
+			}
+		}
+	} else {
+		$_SESSION['shipping'] = false;
+
+		oos_redirect(oos_href_link($aContents['checkout_payment']));
+    }
+}
+
+// get all available shipping quotes
+$quotes = $shipping_modules->quote();
+
+// if no shipping method has been selected, automatically select the cheapest method.
+// if the modules status was changed when none were available, to save on implementing
+// a javascript force-selection method, also automatically select the cheapest shipping
+// method if more than one module is now enabled
+if ((!isset($_SESSION['shipping']) || (!isset($_SESSION['shipping']['id']) || $_SESSION['shipping']['id'] == '') && oos_count_shipping_modules() >= 1)) $_SESSION['shipping'] = $shipping_modules->cheapest();
+
+list ($sess_class, $sess_method) = preg_split('/_/', $_SESSION['shipping']['id']);
+
+
+	
+			/*
+			 * Shopping Cart
+			*/
 			$products = $_SESSION['cart']->get_products();
 
 			$n = count($products);
@@ -105,7 +245,7 @@ if (isset($_SESSION)) {
 						
 					}
 				}
-			}
+			}  
 		}
 	}
 }
