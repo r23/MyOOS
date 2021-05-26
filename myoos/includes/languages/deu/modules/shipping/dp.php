d9b2dab1e2cbea0b7b36e2c443ea5172963da223
r23 <info@r23.de>
2021-05-26 15:18:24 +0200

    Fatal error: Uncaught Error: Undefined constant "MODULE_SHIPPING_DP_COUNTRIES_1_TITLE" in ~/admin/modules.php:191 Stack trace: #0 ~/admin/modules.php(191): constant('MODULE_SHIPPING...') #1 {main} thrown in ~/admin/modules.php on line 191

--- myoos/includes/languages/deu/modules/shipping/dp.php
+++ myoos/includes/languages/deu/modules/shipping/dp.php
@@ -34,38 +34,38 @@ define('MODULE_SHIPPING_DP_ZONE_DESC', 'Geben Sie <strong>einzeln</strong> die Z
 define('MODULE_SHIPPING_DP_SORT_ORDER_TITLE', 'Reihenfolge der Anzeige');
 define('MODULE_SHIPPING_DP_SORT_ORDER_DESC', 'Niedrigste wird zuerst angezeigt.');
 
-define('MODULE_SHIPPING_DP_Länder_1_TITLE', 'DHL Zone 1 Länder');
-define('MODULE_SHIPPING_DP_Länder_1_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 1 sind.');
+define('MODULE_SHIPPING_DP_COUNTRIES_1_TITLE', 'DHL Zone 1 Länder');
+define('MODULE_SHIPPING_DP_COUNTRIES_1_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 1 sind.');
 
 define('MODULE_SHIPPING_DP_COST_1_TITLE', 'DHL Zone 1 Versandkosten');
 define('MODULE_SHIPPING_DP_COST_1_DESC', 'Die Versandkosten für Zone 1 basieren auf einer Reihe von Bestellgewichten. Beispiel: 0-3:8.50,3-7:10.50,.... Gewichte, die größer als 0 und kleiner oder gleich 3 sind, würden 8.50 für Zone 1 kosten');
 
-define('MODULE_SHIPPING_DP_Länder_2_TITLE', 'DHL Zone 2 Länder');
-define('MODULE_SHIPPING_DP_Länder_2_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 2 sind.');
+define('MODULE_SHIPPING_DP_COUNTRIES_2_TITLE', 'DHL Zone 2 Länder');
+define('MODULE_SHIPPING_DP_COUNTRIES_2_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 2 sind.');
 
 define('MODULE_SHIPPING_DP_COST_2_TITLE', 'DHL Zone 2 Versandkosten');
 define('MODULE_SHIPPING_DP_COST_2_DESC', 'Die Versandkosten für Zone 2 basieren auf einer Reihe von Bestellgewichten. Beispiel: 0-3:8.50,3-7:10.50,.... Gewichte, die größer als 0 und kleiner oder gleich 3 sind, würden 8.50 für Zone 2 kosten');
 
-define('MODULE_SHIPPING_DP_Länder_3_TITLE', 'DHL Zone 3 Länder');
-define('MODULE_SHIPPING_DP_Länder_3_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 3 sind.');
+define('MODULE_SHIPPING_DP_COUNTRIES_3_TITLE', 'DHL Zone 3 Länder');
+define('MODULE_SHIPPING_DP_COUNTRIES_3_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 3 sind.');
 
 define('MODULE_SHIPPING_DP_COST_3_TITLE', 'DHL Zone 3 Länder');
 define('MODULE_SHIPPING_DP_COST_3_DESC', 'Die Versandkosten für Zone 3 basieren auf einer Reihe von Bestellgewichten. Beispiel: 0-3:8.50,3-7:10.50,.... Gewichte, die größer als 0 und kleiner oder gleich 3 sind, würden 8.50 für Zone 3 kosten');
 
-define('MODULE_SHIPPING_DP_Länder_4_TITLE', 'DHL Zone 4 Länder');
-define('MODULE_SHIPPING_DP_Länder_4_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 4 sind.');
+define('MODULE_SHIPPING_DP_COUNTRIES_4_TITLE', 'DHL Zone 4 Länder');
+define('MODULE_SHIPPING_DP_COUNTRIES_4_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 4 sind.');
 
 define('MODULE_SHIPPING_DP_COST_4_TITLE', 'DHL Zone 4 Versandkosten');
 define('MODULE_SHIPPING_DP_COST_4_DESC', 'Die Versandkosten für Zone 4 basieren auf einer Reihe von Bestellgewichten. Beispiel: 0-3:8.50,3-7:10.50,.... Gewichte, die größer als 0 und kleiner oder gleich 3 sind, würden 8.50 für Zone 4 kosten.');
 
-define('MODULE_SHIPPING_DP_Länder_5_TITLE', 'DHL Zone 5 Länder');
-define('MODULE_SHIPPING_DP_Länder_5_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 5 sind.');
+define('MODULE_SHIPPING_DP_COUNTRIES_5_TITLE', 'DHL Zone 5 Länder');
+define('MODULE_SHIPPING_DP_COUNTRIES_5_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 5 sind.');
 
 define('MODULE_SHIPPING_DP_COST_5_TITLE', 'DHL Zone 5 Versandkosten');
 define('MODULE_SHIPPING_DP_COST_5_DESC', 'Die Versandkosten für Zone 5 basieren auf einer Reihe von Bestellgewichten. Beispiel: 0-3:8.50,3-7:10.50,.... Gewichte, die größer als 0 und kleiner oder gleich 3 sind, würden 8.50 für Zone 5 kosten');
 
-define('MODULE_SHIPPING_DP_Länder_6_TITLE', 'DHL Zone 6 Länder');
-define('MODULE_SHIPPING_DP_Länder_6_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 6 sind.');
+define('MODULE_SHIPPING_DP_COUNTRIES_6_TITLE', 'DHL Zone 6 Länder');
+define('MODULE_SHIPPING_DP_COUNTRIES_6_DESC', 'Komma getrennte Liste von zweistelligen ISO-Ländercodes, die Teil der Zone 6 sind.');
 
 define('MODULE_SHIPPING_DP_COST_6_TITLE', 'DHL Zone 6 Versandkosten');
 define('MODULE_SHIPPING_DP_COST_6_DESC', 'Die Versandkosten für Zone 6 basieren auf einer Reihe von Bestellgewichten. Beispiel: 0-3:8.50,3-7:10.50,.... Gewichte, die größer als 0 und kleiner oder gleich 3 sind, würden 8.50 für Zone 6 kosten');
