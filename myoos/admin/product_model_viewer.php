7e655830b99f67cadea0374ae832d289bada39fb
r23 <info@r23.de>
2021-07-08 21:36:03 +0200

    Warning: Undefined variable $model_viewer_title in ~/admin/product_model_viewer.php on line 410 Warning: Trying to access array offset on value of type null in ~/admin/product_model_viewer.php on line 410 Warning: Undefined array key "model_viewer_description1" in ~/admin/product_model_viewer.php on line 424 Warning: Undefined array key "model_viewer_description2" in ~/admin/product_model_viewer.php on line 424

--- myoos/admin/product_model_viewer.php
+++ myoos/admin/product_model_viewer.php
@@ -407,7 +407,8 @@ if ($action == 'edit_3d') {
                               <label class="col-lg-2 col-form-label"><?php if ($i == 0) echo TEXT_MODELS_TITLE; ?></label>
 							  <?php if ($nLanguages > 1) echo '<div class="col-lg-1">' .  oos_flag_icon($aLanguages[$i]) . '</div>'; ?>
                               <div class="col-lg-9">
-								<?php echo oos_draw_input_field('model_viewer_title['. $nCounter . '][' . $aLanguages[$i]['id'] . ']', (($model_viewer_title[$aLanguages[$i]['id']]) ? stripslashes($model_viewer_title[$aLanguages[$i]['id']]) : oos_get_model_viewer_title($models['model_viewer_id'], $aLanguages[$i]['id']))); ?>
+								<?php echo oos_draw_input_field('model_viewer_title['. $nCounter . '][' . $aLanguages[$i]['id'] . ']', (isset($model_viewer_title[$aLanguages[$i]['id']]) ? stripslashes($model_viewer_title[$aLanguages[$i]['id']]) : oos_get_model_viewer_title($models['model_viewer_id'], $aLanguages[$i]['id']))); ?>		
+								
                               </div>
                            </div>
                         </fieldset>						
@@ -421,7 +422,8 @@ if ($action == 'edit_3d') {
 							  <?php if ($nLanguages > 1) echo '<div class="col-lg-1">' .  oos_flag_icon($aLanguages[$i]) . '</div>'; ?>
                               <div class="col-lg-9">
 <?php
-       echo oos_draw_textarea_field('model_viewer_description_'. $nCounter . '_' . $aLanguages[$i]['id'], 'soft', '70', '15', ($_POST['model_viewer_description' .$aLanguages[$i]['id']] ? stripslashes($_POST['model_viewer_description' .$aLanguages[$i]['id']]) : oos_get_model_viewer_description($models['model_viewer_id'], $aLanguages[$i]['id'])));
+       echo oos_draw_textarea_field('model_viewer_description_'. $nCounter . '_' . $aLanguages[$i]['id'], 'soft', '70', '15', (isset($_POST['model_viewer_description' .$aLanguages[$i]['id']]) ? stripslashes($_POST['model_viewer_description' .$aLanguages[$i]['id']]) : oos_get_model_viewer_description($models['model_viewer_id'], $aLanguages[$i]['id'])));
+	   
 ?>
                               </div>
                            </div>
