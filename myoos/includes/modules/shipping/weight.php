0704628f95f0a6c706b84e42c9259af2282f189b
r23 <info@r23.de>
2021-07-12 12:51:28 +0200

    isset($oOrder->delivery['country']['id']) &&

--- myoos/includes/modules/shipping/weight.php
+++ myoos/includes/modules/shipping/weight.php
@@ -33,14 +33,14 @@
       $this->icon = '';
       $this->enabled = (defined('MODULE_SHIPPING_WEIGHT_STATUS') && (MODULE_SHIPPING_WEIGHT_STATUS == 'true') ? true : false);
 
-      if ( ($this->enabled == true) && ((defined('MODULE_SHIPPING_WEIGHT_ZONE') && (int)MODULE_SHIPPING_WEIGHT_ZONE > 0)) ) {
+      if ( ($this->enabled == true) && isset($oOrder->delivery['country']['id']) && ((defined('MODULE_SHIPPING_WEIGHT_ZONE') && (int)MODULE_SHIPPING_WEIGHT_ZONE > 0)) ) {
         $check_flag = false;
 
         // Get database information
         $dbconn =& oosDBGetConn();
         $oostable =& oosDBGetTables();
 
-        $check_result = $dbconn->Execute("SELECT zone_id FROM " . $oostable['zones_to_geo_zones'] . " WHERE geo_zone_id = '" . MODULE_SHIPPING_WEIGHT_ZONE . "' AND zone_country_id = '" . $oOrder->delivery['country']['id'] . "' ORDER BY zone_id");
+        $check_result = $dbconn->Execute("SELECT zone_id FROM " . $oostable['zones_to_geo_zones'] . " WHERE geo_zone_id = '" . MODULE_SHIPPING_WEIGHT_ZONE . "' AND zone_country_id = '" . intval($oOrder->delivery['country']['id']) . "' ORDER BY zone_id");
         while ($check = $check_result->fields) {
           if ($check['zone_id'] < 1) {
             $check_flag = true;
