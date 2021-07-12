2f43441a4d9c9b79953af81062b15d5d59eb78d7
r23 <info@r23.de>
2021-07-12 12:33:21 +0200

    Warning: Attempt to read property "delivery" on null in ~/includes/modules/shipping/fedexeu.php on line 66 Warning: Trying to access array offset on value of type null in ~/includes/modules/shipping/fedexeu.php on line 66 Warning: Trying to access array offset on value of type null in ~/includes/modules/shipping/fedexeu.php on line 66

--- myoos/includes/modules/shipping/fedexeu.php
+++ myoos/includes/modules/shipping/fedexeu.php
@@ -55,7 +55,7 @@
       $this->icon = OOS_ICONS . 'shipping_fedexeu.gif';
       $this->enabled = (defined('MODULE_SHIPPING_FEDEXEU_STATUS') && (MODULE_SHIPPING_FEDEXEU_STATUS == 'true') ? true : false);
 
-      if ( ($this->enabled == true) && ((int)MODULE_SHIPPING_FEDEXEU_ZONE > 0) ) {
+      if ( ($this->enabled == true) && isset($oOrder->delivery['country']['id']) && ((int)MODULE_SHIPPING_FEDEXEU_ZONE > 0) ) {
         $check_flag = false;
 
         // Get database information
@@ -63,7 +63,7 @@
         $oostable =& oosDBGetTables();
 
         $zones_to_geo_zonestable = $oostable['zones_to_geo_zones'];
-        $check_result = $dbconn->Execute("SELECT zone_id FROM $zones_to_geo_zonestable WHERE geo_zone_id = '" . MODULE_SHIPPING_FEDEXEU_ZONE . "' AND zone_country_id = '" . $oOrder->delivery['country']['id'] . "' ORDER BY zone_id");
+        $check_result = $dbconn->Execute("SELECT zone_id FROM $zones_to_geo_zonestable WHERE geo_zone_id = '" . MODULE_SHIPPING_FEDEXEU_ZONE . "' AND zone_country_id = '" . intval($oOrder->delivery['country']['id']) . "' ORDER BY zone_id");
         while ($check = $check_result->fields) {
           if ($check['zone_id'] < 1) {
             $check_flag = true;
