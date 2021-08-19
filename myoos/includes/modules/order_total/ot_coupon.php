7a74d8c606f13a40e1d7cac357a392b0b9e89c8f
r23 <info@r23.de>
2021-08-19 16:17:51 +0200

    update ot_coupon

--- myoos/includes/modules/order_total/ot_coupon.php
+++ myoos/includes/modules/order_total/ot_coupon.php
@@ -54,7 +54,7 @@ class ot_coupon {
 
 		if ($od_amount > 0) {
 			$oOrder->info['total'] = $oOrder->info['total'] - $od_amount;
-			$this->output[] = array('title' => '<font color="#FF0000">' . $this->title . ':' . $this->coupon_code .':</font>',
+			$this->output[] = array('title' => '<font color="#FF0000">' . $this->title . ' ' . $this->coupon_code .':</font>',
 								'text' => '<strong><font color="#FF0000"> - ' . $oCurrencies->format($od_amount) . '</font></strong>',
 								'info' => '',
 								'value' => $od_amount);
@@ -73,12 +73,13 @@ class ot_coupon {
 	
 			$currency_type = (isset($_SESSION['currency']) ? $_SESSION['currency'] : DEFAULT_CURRENCY);		
 			$currency_value = $oCurrencies->currencies[$_SESSION['currency']]['value'];
-		
+
 			if ($od_amount > 0) {
+				$this->calculate_tax_deduction($total, $od_amount);
 				$_SESSION['cart']->info['total'] = $_SESSION['cart']->info['total'] - $od_amount;
 									
-				$this->output[] = array('title' => '<span class="text-danger">' . $this->title . ':' . $this->coupon_code . ':</span>',
-                                'text' => '<span class="text-danger"><strong>' .  $oCurrencies->format($shipping_cost, true, $currency_type, $currency_value) . '</strong></span>',
+				$this->output[] = array('title' => '<span class="text-danger">' . $this->title . ' ' . $this->coupon_code . ':</span>',
+                                'text' => '<span class="text-danger"><strong>-' .  $oCurrencies->format($od_amount, true, $currency_type, $currency_value) . '</strong></span>',
 								'info' => '',
                                 'value' => $od_amount);									
 			}
@@ -442,6 +443,209 @@ class ot_coupon {
 		}
 		return $od_amount;
 	}
+
+  function calculate_tax_deduction($amount, $od_amount, $method) {
+
+    // Get database information
+    $dbconn =& oosDBGetConn();
+    $oostable =& oosDBGetTables();
+
+	if (isset($_SESSION['cc_id'])) {
+		$cc_id = intval($_SESSION['cc_id']);
+
+		$couponstable = $oostable['coupons'];
+		$sql = "SELECT coupon_code coupon_id, coupon_amount, coupon_type, coupon_minimum_order,
+					uses_per_coupon, uses_per_user, restrict_to_products,
+					restrict_to_categories 
+				FROM $couponstable
+				WHERE coupon_id = '" . intval($cc_id). "'
+					AND coupon_active = 'Y'";
+		$coupon_query = $dbconn->Execute($sql);
+
+		if ($coupon_query->RecordCount() !=0 ) {
+			$coupon_result = $coupon_query->fields;
+
+			$this->coupon_code = $coupon_result['coupon_code'];
+				
+			if ($coupon_result['coupon_minimum_order'] <= $amount) {
+
+		
+				if ($coupon_result['coupon_type'] == 'S') {
+
+					// Sales tax recalculation for shipping costs
+
+/*
+					$c_deduct = $_SESSION['shipping']['cost'];
+			$_SESSION['cart']->info['total'] += $_SESSION['shipping']['cost'];
+			
+			$subtotal = $_SESSION['cart']->info['subtotal'];
+			
+			$tax = $_SESSION['cart']->info['tax'];
+			if ($aUser['price_with_tax'] == 1) {
+				$subtotal = $subtotal - $tax;
+			}
+		
+			$sales_tax_rates = count($_SESSION['cart']->info['net_total']);
+			reset($_SESSION['cart']->info['net_total']);
+			
+			foreach($_SESSION['cart']->info['net_total'] as $key => $value) {		  
+				if ($value > 0) {
+					$share =  $value * 100 / $subtotal;
+					$shipping_cost = $_SESSION['shipping']['cost'] * $share / 100;
+					$tax = $shipping_cost - oos_round((($shipping_cost * 100) / (100 + $key)), 2);
+
+					$_SESSION['cart']->info['tax'] += $tax;
+					$_SESSION['cart']->info['tax_groups']["$key"] += $tax;
+					
+					if ($sales_tax_rates > 1) {									
+						$info = sprintf($aLang['tax_incl_available_from'], number_format($key, 2).'%');
+						$info = '<br><small> (' . $info . '): </small>';
+					} else {
+						$info = '';
+					}
+					
+					$this->output[] = array('title' => $_SESSION['shipping']['title'] . $info,
+										'text' => $oCurrencies->format($shipping_cost, true, $currency_type, $currency_value),
+										'info' => '',
+										'value' => $shipping_cost);						
+					
+					
+				}
+			}
+
+*/
+
+
+/*
+
+    if ($coupon_query->RecordCount() !=0 ) {
+      $coupon_result = $coupon_query->fields;
+      $coupon_get = $dbconn->Execute("SELECT coupon_amount, coupon_minimum_order, restrict_to_products, restrict_to_categories, coupon_type FROM " . $oostable['coupons'] . " WHERE coupon_code = '". $coupon_result['coupon_code'] . "'");
+      $get_result = $coupon_get->fields;
+      if ($get_result['coupon_type'] != 'S') {
+      if ($get_result['restrict_to_products'] || $get_result['restrict_to_categories']) {
+        // What to do here.
+        // Loop through all products and build a list of all product_ids, price, tax class
+        // at the same time create total net amount.
+        // then
+        // for percentage discounts. simply reduce tax group per product by discount percentage
+        // or
+        // for fixed payment amount
+        // calculate ratio based on total net 
+        // for each product reduce tax group per product by ratio amount.
+        $products = $_SESSION['cart']->get_products();
+        for ($i=0; $i<count($products); $i++) {
+          $t_prid = oos_get_product_id($products[$i]['id']);
+
+          $productstable = $oostable['products'];
+          $cc_query = $dbconn->Execute("SELECT products_tax_class_id FROM $productstable WHERE products_id = '" . (int)$t_prid . "'");
+          $cc_result = $cc_query->fields;
+          $valid_product = false;
+
+          if ($get_result['restrict_to_products']) {
+            $pr_ids = preg_split("/[,]/", $get_result['restrict_to_products']);
+            for ($p = 0; $p < count($pr_ids); $p++) {
+              if ($pr_ids[$p] == $t_prid) $valid_product = true;
+            }
+          }
+          if ($get_result['restrict_to_categories']) {
+            $cat_ids = preg_split("/[,]/", $get_result['restrict_to_categories']);
+            for ($c = 0; $c < count($cat_ids); $c++) {
+
+              $products_to_categoriestable = $oostable['products_to_categories'];
+              $cat_query = $dbconn->Execute("SELECT products_id FROM $products_to_categoriestable WHERE products_id = '" . (int)$products_id . "' AND categories_id = '" . (int)$cat_ids[$i] . "'");
+              if ($cat_query->RecordCount() !=0 ) $valid_product = true;
+            }
+          }
+
+          if ($valid_product) {
+            $valid_array[] = array('product_id' => $t_prid,
+                                   'products_price' => $products[$i]['final_price'] * $products[$i]['quantity'],
+                                   'products_tax_class' => $cc_result['products_tax_class_id']);
+            $total_price += $products[$i]['final_price'] * $products[$i]['quantity'];
+          }
+        }
+        if ($valid_product) {
+        if ($get_result['coupon_type'] == 'P') {
+          $ratio = $get_result['coupon_amount']/100;
+        } else {
+          $ratio = $od_amount / $total_price;
+        }
+        if ($get_result['coupon_type'] == 'S') $ratio = 1; 
+          if ($method=='Credit Note') {
+            $tax_rate = oos_get_tax_rate($this->tax_class, $oOrder->delivery['country']['id'], $oOrder->delivery['zone_id']);
+            $tax_desc = oos_get_tax_description($this->tax_class, $oOrder->delivery['country']['id'], $oOrder->delivery['zone_id']);
+            if ($get_result['coupon_type'] == 'P') {
+              $tod_amount = $od_amount / (100 + $tax_rate)* $tax_rate;
+            } else {
+              $tod_amount = $oOrder->info['tax_groups'][$tax_desc] * $od_amount/100;
+            }
+            $oOrder->info['tax_groups'][$tax_desc] -= $tod_amount;
+            $oOrder->info['total'] -= $tod_amount;
+          } else {     
+            for ($p=0; $p<count($valid_array); $p++) {
+              $tax_rate = oos_get_tax_rate($valid_array[$p]['products_tax_class'], $oOrder->delivery['country']['id'], $oOrder->delivery['zone_id']);
+              $tax_desc = oos_get_tax_description($valid_array[$p]['products_tax_class'], $oOrder->delivery['country']['id'], $oOrder->delivery['zone_id']);
+              if ($tax_rate > 0) {
+                $tod_amount[$tax_desc] += ($valid_array[$p]['products_price'] * $tax_rate)/100 * $ratio;
+                $oOrder->info['tax_groups'][$tax_desc] -= ($valid_array[$p]['products_price'] * $tax_rate)/100 * $ratio;
+                $oOrder->info['total'] -= ($valid_array[$p]['products_price'] * $tax_rate)/100 * $ratio;
+              }
+            }
+          }
+        }
+      } else {
+        if ($get_result['coupon_type'] =='F') {
+          $tod_amount = 0;
+          if ($method=='Credit Note') {
+            $tax_rate = oos_get_tax_rate($this->tax_class, $oOrder->delivery['country']['id'], $oOrder->delivery['zone_id']);
+            $tax_desc = oos_get_tax_description($this->tax_class, $oOrder->delivery['country']['id'], $oOrder->delivery['zone_id']);
+            $tod_amount = $od_amount / (100 + $tax_rate)* $tax_rate;
+            $oOrder->info['tax_groups'][$tax_desc] -= $tod_amount;
+          } else {  
+            $ratio1 = $od_amount/$amount;
+            reset($oOrder->info['tax_groups']);
+            foreach($oOrder->info['tax_groups'] as $key => $value) {				
+              $tax_rate = oos_get_tax_rate_from_desc($key);
+              $net = $tax_rate * $oOrder->info['tax_groups'][$key];
+              if ($net>0) {
+                $god_amount = $oOrder->info['tax_groups'][$key] * $ratio1;
+                $tod_amount += $god_amount;
+                $oOrder->info['tax_groups'][$key] = $oOrder->info['tax_groups'][$key] - $god_amount;
+              }
+            }
+          }
+          $oOrder->info['total'] -= $tod_amount;
+        }
+        if ($get_result['coupon_type'] =='P') {
+          $tod_amount=0;
+          if ($method=='Credit Note') {
+            $tax_desc = oos_get_tax_description($this->tax_class, $oOrder->delivery['country']['id'], $oOrder->delivery['zone_id']);
+            $tod_amount = $oOrder->info['tax_groups'][$tax_desc] * $od_amount/100;
+            $oOrder->info['tax_groups'][$tax_desc] -= $tod_amount;
+          } else {
+            reset($oOrder->info['tax_groups']);
+            foreach($oOrder->info['tax_groups'] as $key => $value) {				
+              $god_amout=0;
+              $tax_rate = oos_get_tax_rate_from_desc($key);
+              $net = $tax_rate * $oOrder->info['tax_groups'][$key];
+              if ($net>0) {
+                $god_amount = $oOrder->info['tax_groups'][$key] * $get_result['coupon_amount']/100;
+                $tod_amount += $god_amount;
+                $oOrder->info['tax_groups'][$key] = $oOrder->info['tax_groups'][$key] - $god_amount;
+              }
+            }
+          }
+          $oOrder->info['tax'] -= $tod_amount;
+        }
+      }
+    }
+    }
+    return $tod_amount;
+*/
+
+  }
+
 
 
  function update_credit_account($i) {
