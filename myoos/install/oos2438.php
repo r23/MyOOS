446b1003d362f14d259efe7a277e374c2e62aa1d
r23 <info@r23.de>
2021-11-10 16:23:19 +0100

    import update script for MyOOS 2.4.38

---
+++ myoos/install/oos2438.php
@@ -0,0 +1,40 @@
+﻿<?php
+/* ----------------------------------------------------------------------
+   $Id: oos160.php,v 1.3 2009/01/13 21:29:21 r23 Exp $
+
+   MyOOS [Shopsystem]
+   https://www.oos-shop.de
+
+   Copyright (c) 2003 - 2021 by the MyOOS Development Team
+   ----------------------------------------------------------------------
+   Based on:
+
+   File: pn64.php,v 1.45 2002/03/16 15:24:37 johnnyrocket
+   ----------------------------------------------------------------------
+   POST-NUKE Content Management System
+   Copyright (C) 2001 by the Post-Nuke Development Team.
+   http://www.postnuke.com/
+   ----------------------------------------------------------------------
+   osCommerce, Open Source E-Commerce Solutions
+   http://www.oscommerce.com
+
+   Copyright (c) 2002 - 2003 osCommerce
+   ----------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------- */
+
+global $db, $prefix_table, $currentlang;
+
+if (!$prefix_table == '') $prefix_table = $prefix_table . '_';
+
+$today = date("Y-m-d H:i:s");
+
+
+$table = $prefix_table . 'configuration';
+$result = $db->Execute("INSERT INTO " . $table . " (configuration_key, configuration_value, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES ('TAKE_BACK_OBLIGATION', '', 1, 22, NULL, " . $db->DBTimeStamp($today) . ", NULL, NULL)");
+if ($result === false) {
+	echo '<br /><img src="images/no.gif" alt="" border="0" align="absmiddle">&nbsp;<font class="oos-error">' .  $db->ErrorMsg() . NOTMADE . '</font>';
+} else {
+  echo '<br /><img src="images/yes.gif" alt="" border="0" align="absmiddle">&nbsp;<font class="oos-title">' . $table . ' ' . UPDATED .'</font>';
+}
+
