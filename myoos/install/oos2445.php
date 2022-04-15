a32cead47a1ee375c68f82b188738a9ac73cc088
r23 <info@r23.de>
2022-04-15 15:09:39 +0200

    Failed opening 'oos2445.php' for inclusion

---
+++ myoos/install/oos2445.php
@@ -0,0 +1,33 @@
+<?php
+/* ----------------------------------------------------------------------
+   $Id: oos160.php,v 1.3 2009/01/13 21:29:21 r23 Exp $
+
+   MyOOS [Shopsystem]
+   https://www.oos-shop.de
+
+   Copyright (c) 2003 - 2021 by the MyOOS Development Team
+   ----------------------------------------------------------------------
+   Based on:
+
+   File: pn64.php,v 1.45 2002/03/16 15:24:37 johnnyrocket
+   ----------------------------------------------------------------------
+   POST-NUKE Content Management System
+   Copyright (C) 2001 by the Post-Nuke Development Team.
+   http://www.postnuke.com/
+   ----------------------------------------------------------------------
+   osCommerce, Open Source E-Commerce Solutions
+   http://www.oscommerce.com
+
+   Copyright (c) 2002 - 2003 osCommerce
+   ----------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------- */
+
+global $db, $prefix_table, $currentlang;
+
+if (!$prefix_table == '') {
+    $prefix_table = $prefix_table . '_';
+}
+
+include_once 'oostables2445.php';
+
