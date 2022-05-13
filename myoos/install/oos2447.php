7728a322bf714c537c2eb700c27ac0caa501fa45
r23 <info@r23.de>
2022-05-13 14:49:05 +0200

    import update Script for 2.4.47

---
+++ myoos/install/oos2447.php
@@ -0,0 +1,48 @@
+<?php
+/* ----------------------------------------------------------------------
+   $Id: oos160.php,v 1.3 2009/01/13 21:29:21 r23 Exp $
+
+   MyOOS [Shopsystem]
+   https://www.oos-shop.de
+
+   Copyright (c) 2003 - 2021 by the MyOOS Development Team
+   ----------------------------------------------------------------------
+   Based on:
+
+   File: pn64.php,v 1.45 2002/03/16 15:24:37 johnnyrocket
+   ----------------------------------------------------------------------
+   POST-NUKE Content Management System
+   Copyright (C) 2001 by the Post-Nuke Development Team.
+   http://www.postnuke.com/
+   ----------------------------------------------------------------------
+   osCommerce, Open Source E-Commerce Solutions
+   http://www.oscommerce.com
+
+   Copyright (c) 2002 - 2003 osCommerce
+   ----------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------- */
+
+global $db, $prefix_table, $currentlang;
+
+if (!$prefix_table == '') {
+    $prefix_table = $prefix_table . '_';
+}
+
+
+// specials
+$table = $prefix_table . 'specials';
+$result = $db->Execute("ALTER TABLE  " . $table . " ADD `specials_cross_out_price` DECIMAL(10,4) NULL DEFAULT NULL AFTER `specials_new_products_price`");
+if ($result === false) {
+    echo '<br /><img src="images/no.gif" alt="" border="0" align="absmiddle">&nbsp;<font class="oos-error">' .  $db->ErrorMsg() . NOTMADE . '</font>';
+} else {
+    echo '<br /><img src="images/yes.gif" alt="" border="0" align="absmiddle">&nbsp;<font class="oos-title">' . $table . ' ' . UPDATED .'</font>';
+}
+
+$result = $db->Execute("ALTER TABLE  " . $table . " ADD `specials_start_date` DATETIME NOT NULL AFTER `specials_date_added`");
+if ($result === false) {
+    echo '<br /><img src="images/no.gif" alt="" border="0" align="absmiddle">&nbsp;<font class="oos-error">' .  $db->ErrorMsg() . NOTMADE . '</font>';
+} else {
+    echo '<br /><img src="images/yes.gif" alt="" border="0" align="absmiddle">&nbsp;<font class="oos-title">' . $table . ' ' . UPDATED .'</font>';
+}
+
