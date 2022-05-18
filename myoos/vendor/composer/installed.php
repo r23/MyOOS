266663c0f10845e12db075d16fdeb1bec8b0d6ed
r23 <info@r23.de>
2022-05-18 03:37:26 +0200

    - Upgrading smarty/smarty (v4.1.0 => v4.1.1)

--- myoos/vendor/composer/installed.php
+++ myoos/vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '877a793b81284cf19edd299d0afd6ed0acf4b406',
+        'reference' => '3da8e7672c76e9fbfb734685d021a201eea28758',
         'name' => 'myoos/myoos',
         'dev' => true,
     ),
@@ -178,7 +178,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '877a793b81284cf19edd299d0afd6ed0acf4b406',
+            'reference' => '3da8e7672c76e9fbfb734685d021a201eea28758',
             'dev_requirement' => false,
         ),
         'nesbot/carbon' => array(
@@ -433,12 +433,12 @@
             'dev_requirement' => false,
         ),
         'smarty/smarty' => array(
-            'pretty_version' => 'v4.1.0',
-            'version' => '4.1.0.0',
+            'pretty_version' => 'v4.1.1',
+            'version' => '4.1.1.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../smarty/smarty',
             'aliases' => array(),
-            'reference' => '9e0536de18b53ba193364291ef0303b0ab9903e1',
+            'reference' => '71036be8be02bf93735c47b0b745f722efbc729f',
             'dev_requirement' => false,
         ),
         'stripe/stripe-php' => array(
