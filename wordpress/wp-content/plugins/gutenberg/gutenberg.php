3e90a3be58ec89e8b28e11904b87f584c97d0689
r23 <info@r23.de>
2022-05-21 16:00:49 +0200

    update Gutenberg

--- wordpress/wp-content/plugins/gutenberg/gutenberg.php
+++ wordpress/wp-content/plugins/gutenberg/gutenberg.php
@@ -5,7 +5,7 @@
  * Description: Printing since 1440. This is the development plugin for the new block editor in core.
  * Requires at least: 5.8
  * Requires PHP: 5.6
- * Version: 13.2.1
+ * Version: 13.2.2
  * Author: Gutenberg Team
  * Text Domain: gutenberg
  *
@@ -13,8 +13,8 @@
  */
 
 ### BEGIN AUTO-GENERATED DEFINES
-define( 'GUTENBERG_VERSION', '13.2.1' );
-define( 'GUTENBERG_GIT_COMMIT', 'f2ac96096d487869b4348b6e106f10dad0ca8704' );
+define( 'GUTENBERG_VERSION', '13.2.2' );
+define( 'GUTENBERG_GIT_COMMIT', '26252bf5426210d2eb738715fdbfc0130a284cef' );
 ### END AUTO-GENERATED DEFINES
 
 gutenberg_pre_init();
