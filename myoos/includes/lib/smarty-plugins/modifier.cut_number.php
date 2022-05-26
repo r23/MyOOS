758abf7f27b93b2518fe5e7e5638ae90b2334b61
r23 <info@r23.de>
2022-05-26 16:07:14 +0200

    import cut_number

---
+++ myoos/includes/lib/smarty-plugins/modifier.cut_number.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+
+/**
+ * Smarty cut_number modifier plugin
+ *
+ * Type:     modifier<br>
+ * Name:     cut_number<br>
+ * @param string
+ * @param string
+ * @return string
+ */
+function smarty_modifier_cut_number($number)
+{
+	$number = explode(".", $number, 2);
+
+	if ($number[1] == '0000') {
+		return $number[0];
+	}	
+  
+	return $number;
+
+}
