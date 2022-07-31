81671ffb048714de1ab52e87aaf179be74c1aa47
r23 <info@r23.de>
2022-07-31 16:01:49 +0200

    import update script for 2.4.51

---
+++ myoos/install/oos2451.php
@@ -0,0 +1,40 @@
+<?php
+/* ----------------------------------------------------------------------
+   $Id: oos160.php,v 1.3 2009/01/13 21:29:21 r23 Exp $
+
+   MyOOS [Shopsystem]
+   https://www.oos-shop.de
+
+   Copyright (c) 2003 - 2021 by the MyOOS Development Team
+   ----------------------------------------------------------------------
+   Based on:
+
+   File: pn64.php,v 1.45 2002/03/16 15:24:37 johnnyrocket
+   ----------------------------------------------------------------------
+   POST-NUKE Content Management System
+   Copyright (C) 2001 by the Post-Nuke Development Team.
+   http://www.postnuke.com/
+   ----------------------------------------------------------------------
+   osCommerce, Open Source E-Commerce Solutions
+   http://www.oscommerce.com
+
+   Copyright (c) 2002 - 2003 osCommerce
+   ----------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------- */
+
+global $db, $prefix_table, $currentlang;
+
+if (!$prefix_table == '') {
+    $prefix_table = $prefix_table . '_';
+}
+
+
+$table = $prefix_table . 'products_models';
+$result = $db->Execute("ALTER TABLE " . $table . " ADD COLUMN `models_object_scalar` VARCHAR(4) AFTER `models_object_rotation`");
+if ($result === false) {
+    echo '<br /><img src="images/no.gif" alt="" border="0" align="absmiddle">&nbsp;<font class="oos-error">' .  $db->ErrorMsg() . NOTMADE . '</font>';
+} else {
+    echo '<br /><img src="images/yes.gif" alt="" border="0" align="absmiddle">&nbsp;<font class="oos-title">' . $table . ' ' . UPDATED .'</font>';
+}
+
