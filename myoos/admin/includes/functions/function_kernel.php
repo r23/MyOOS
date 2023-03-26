f99a35af13ad4909aa14992f912e0702531bd825
r23 <info@r23.de>
2023-03-26 15:06:45 +0200

    misc

--- myoos/admin/includes/functions/function_kernel.php
+++ myoos/admin/includes/functions/function_kernel.php
@@ -970,6 +970,24 @@ function oos_get_tax_rate($class_id, $country_id = -1, $zone_id = -1)
                 AND tr.tax_class_id = '" . intval($class_id) . "'";
     $result = $dbconn->Execute($query);
 
+echo 'Ralf';
+RalfSELECT SUM(tax_rate) AS tax_rate
+              FROM dwq_tax_rates tr LEFT JOIN
+                   dwq_zones_to_geo_zones za
+                 ON (tr.tax_zone_id = za.geo_zone_id) LEFT JOIN
+                   dwq_geo_zones tz
+                 ON (tz.geo_zone_id = tr.tax_zone_id)
+              WHERE (za.zone_country_id IS null or za.zone_country_id = '0'
+                  OR za.zone_country_id = '81')
+                AND (za.zone_id is null OR za.zone_id = '0'
+                  OR za.zone_id = '88')
+                AND tr.tax_class_id = '2
+
+
+echo $query;
+print_r($result);
+exit;
+
     if ($result->RecordCount()) {
         $tax_multiplier = 0;
         while ($tax = $result->fields) {
--- myoos/includes/classes/class_shopping_cart.php
+++ myoos/includes/classes/class_shopping_cart.php
@@ -350,7 +350,7 @@ class shoppingCart
                         if (trim($value) == null) {
                             $blank_value = true;
                         } else {
-                            $option = substr($option, strlen(TEXT_PREFIX));
+                            $option = substr($option, strlen(TEXT_PREFIX ?? ''));
                             // $attr_value = htmlspecialchars(stripslashes((string)$value), ENT_QUOTES, 'UTF-8');
                             $attr_value = stripslashes($value);
                             $value = PRODUCTS_OPTIONS_VALUE_TEXT_ID;
