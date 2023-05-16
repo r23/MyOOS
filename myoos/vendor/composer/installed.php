ae1dbb73145ebd70e875ce54ea851b6d18fe682f
r23 <info@r23.de>
2023-05-16 14:54:36 +0200

    compoer update

--- myoos/vendor/composer/installed.php
+++ myoos/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'myoos/myoos',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '756119c8e6b6e56bf4b6dfa53dec0bfcba0fedc9',
+        'reference' => 'f79c1fe8707b1f3126a1a7644c13ccb800ecbc15',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -193,7 +193,7 @@
         'myoos/myoos' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '756119c8e6b6e56bf4b6dfa53dec0bfcba0fedc9',
+            'reference' => 'f79c1fe8707b1f3126a1a7644c13ccb800ecbc15',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
