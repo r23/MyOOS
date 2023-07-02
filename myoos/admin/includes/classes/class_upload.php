c290e6d3bc569d4d7a1c046a1b304828a09d61a1
r23 <info@r23.de>
2023-07-02 14:41:22 +0200

    Fatal error: Uncaught Error: Call to undefined function ttrim() in /class_upload.php:628

--- myoos/admin/includes/classes/class_upload.php
+++ myoos/admin/includes/classes/class_upload.php
@@ -625,7 +625,7 @@ class upload
         // Remove path information and dots around the filename, to prevent uploading
         // into different directories or replacing hidden system files.
         // Also remove control characters and spaces (\x00..\x20) around the filename:
-        $name = ttrim((string) $this->basename(stripslashes($name)), ".\x00..\x20");
+        $name = trim((string) $this->basename(stripslashes($name)), ".\x00..\x20");
         // Replace dots in filenames to avoid security issues with servers
         // that interpret multiple file extensions, e.g. "example.php.png":
         $replacement = $this->options['replace_dots_in_filenames'];
