f53a960a48cbb0a91b767e133146f64ae6a3e97f
r23 <info@r23.de>
2023-07-23 21:33:09 +0200

    update script for oos2458

---
+++ myoos/install/oos2458.php
@@ -0,0 +1,45 @@
+<?php
+/**
+   ----------------------------------------------------------------------
+
+   MyOOS [Shopsystem]
+   https://www.oos-shop.de
+
+   Copyright (c) 2003 - 2023 by the MyOOS Development Team.
+   ----------------------------------------------------------------------
+   Based on:
+
+   File: pn64.php,v 1.45 2002/03/16 15:24:37 johnnyrocket
+   ----------------------------------------------------------------------
+   POST-NUKE Content Management System
+   Copyright (C) 2001 by the Post-Nuke Development Team.
+   http://www.postnuke.com/
+   ----------------------------------------------------------------------
+   osCommerce, Open Source E-Commerce Solutions
+   http://www.oscommerce.com
+
+   Copyright (c) 2002 - 2003 osCommerce
+   ----------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------- */
+
+global $db, $prefix_table, $currentlang;
+
+if (!$prefix_table == '') {
+    $prefix_table = $prefix_table . '_';
+}
+
+$today = date("Y-m-d H:i:s");
+
+
+require_once 'oostables2458.php';
+
+# Todo: remove gif and font tag
+#
+# $table = $prefix_table . 'configuration';
+# $result = $db->Execute("INSERT INTO " . $table . " (configuration_key, configuration_value, configuration_group_id, sort_order, last_modified, date_added, use_function, set_function) VALUES ('PRODUCTS_CHARTS', 'true', 5, 12, NULL, " . $db->DBTimeStamp($today) . ", NULL, 'oos_cfg_select_option(array(\'true\', \'false\'),')");
+# if ($result === false) {
+#    echo '<br /><img src="images/no.gif" alt="" border="0" align="absmiddle">&nbsp;<font class="oos-error">' .  $db->ErrorMsg() . NOTMADE . '</font>';
+# } else {
+#    echo '<br /><img src="images/yes.gif" alt="" border="0" align="absmiddle">&nbsp;<font class="oos-title">' . $table . ' ' . UPDATED .'</font>';
+# }
---
+++ myoos/install/oostables2458.php
@@ -0,0 +1,90 @@
+<?php
+/**
+   ----------------------------------------------------------------------
+
+   MyOOS [Shopsystem]
+   https://www.oos-shop.de
+
+   Copyright (c) 2003 - 2023 by the MyOOS Development Team.
+   ----------------------------------------------------------------------
+   Based on:
+
+   POST-NUKE Content Management System
+   Copyright (C) 2001 by the Post-Nuke Development Team.
+   http://www.postnuke.com/
+   ----------------------------------------------------------------------
+   osCommerce, Open Source E-Commerce Solutions
+   http://www.oscommerce.com
+
+   Copyright (c) 2002 - 2003 osCommerce
+   ----------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------- */
+
+function dosql($table, $flds)
+{
+    global $db;
+
+    $dict = NewDataDictionary($db);
+
+    // $dict->debug = 1;
+    $taboptarray = array('mysql' => 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;', 'REPLACE');
+
+    $sqlarray = $dict->createTableSQL($table, $flds, $taboptarray);
+    $dict->executeSqlArray($sqlarray);
+
+
+
+    echo '<br /><img src="images/yes.gif" alt="" border="0" align="absmiddle"> <font class="oos-title">' . $table . " " . MADE . '</font>';
+}
+
+function idxsql($idxname, $table, $idxflds)
+{
+    global $db;
+
+    $dict = NewDataDictionary($db);
+
+    $sqlarray = $dict->CreateIndexSQL($idxname, $table, $idxflds);
+    $dict->executeSqlArray($sqlarray);
+}
+
+
+
+
+$table = $prefix_table . 'products_price_alarm';
+$flds = "
+  products_price_alarm_id I NOTNULL AUTO PRIMARY,
+  products_id I NOTNULL PRIMARY,
+  price_alarm_recipients_id I NOTNULL PRIMARY,
+  products_price N '10.4' NOTNULL DEFAULT '0.0000',
+  date_added T
+";
+dosql($table, $flds);
+
+
+$table = $prefix_table . 'products_price_alarm_recipients';
+$flds = "
+  price_alarm_recipients_id I NOTNULL AUTO PRIMARY,
+  price_alert_receiver_email_address C(96) NOTNULL,
+  price_alert_receiver_password C(255),	
+  customers_id I,
+  date_added T,
+  mail_key C(32) NOTNULL,
+  mail_sha1 C(232) NOTNULL,
+  key_sent T,
+  status I1 DEFAULT '0'
+";
+dosql($table, $flds);
+
+
+$table = $prefix_table . 'products_price_alarm_history';
+$flds = "
+  price_alarm_recipients_status_history_id I NOTNULL AUTO PRIMARY,
+  price_alarm_recipients_id I NOTNULL DEFAULT '0',
+  new_value I1 NOTNULL DEFAULT '0',
+  old_value I1 DEFAULT NULL,
+  date_added T,
+  customer_notified I1 DEFAULT '0'
+";
+dosql($table, $flds);
+
