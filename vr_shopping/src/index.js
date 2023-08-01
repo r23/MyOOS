5acffb9f469d8f7038433aaf1aaf337df2d696ea
r23 <info@r23.de>
2023-08-01 16:19:41 +0200

    import enable3d

--- vr_shopping/src/index.js
+++ vr_shopping/src/index.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import * as AFRAME from "aframe";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { PhysicsLoader } from "enable3d";
+import { AmmoPhysics } from "enable3d/node_modules/@enable3d/ammo-physics";
 
 require('aframe-extras');
 
