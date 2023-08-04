c0b635e1fc5f91b37c4906aa7ef82ceb04f84036
r23 <info@r23.de>
2023-08-05 01:03:05 +0200

    npm install @react-three/drei

--- metaverse-experience/src/index.js
+++ metaverse-experience/src/index.js
@@ -31,7 +31,20 @@ const loader = new GLTFLoader();
 const clock = new THREE.Clock();
 
 
-const title = 'React with Webpack and Babel';
+AFRAME.registerPrimitive('a-ocean', {
+  // Attaches the `ocean` component by default.
+  // Defaults the ocean to be parallel to the ground.
+  defaultComponents: {
+    ocean: {},
+    rotation: {x: -90, y: 0, z: 0}
+  },
 
-let greeting = " --- Hallo Welt!---"
-console.log(greeting.length)
+  // Maps HTML attributes to the `ocean` component's properties.
+  mappings: {
+    width: 'ocean.width',
+    depth: 'ocean.depth',
+    density: 'ocean.density',
+    color: 'ocean.color',
+    opacity: 'ocean.opacity'
+  }
+});
