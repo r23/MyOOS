196381a9ce106c40c0a0c063bd5f525a93afd4ca
r23 <info@r23.de>
2023-08-04 06:00:11 +0200

    import Metaverse

---
+++ metaverse-experience/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as THREE from "three";
+import * as AFRAME from "aframe";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Canvas, useFrame } from '@react-three/fiber';
+
+
+require('aframe-extras');
+
+
+let scene;
+let wraper;
+let camera;
+let cursor;
+
+let gamepad;
+
+let physics;
+let camera_physics_object;
+
+let sphere_radius = 0.5;
+let camera_height = 1;
+let ready = false;
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader();
+const clock = new THREE.Clock();
+
+
+const title = 'React with Webpack and Babel';
+
--- metaverse-experience/webpack.config.js
+++ metaverse-experience/webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: path.resolve(__dirname, './src/index.js'),
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, 'dist'),
     },
     target: 'node'
 };
+
