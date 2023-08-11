c4a73a45d4f0bd8c7cbc3338c420fe210687f884
r23 <info@r23.de>
2023-08-12 00:31:47 +0200

    return to a-frame

--- metaverse-experience/src/index.js
+++ metaverse-experience/src/index.js
@@ -1,15 +1,9 @@
-// Als Module importieren
 import * as THREE from 'three';
 import * as AFRAME from 'aframe';
 
-// Aus dem heruntergeladenen Ordner importieren
 // Aus dem Three.js-Paket importieren
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
-
-
-// import { Canvas, useFrame } from '@react-three/fiber';
 
 require('aframe-extras');
 require('aframe-physics-system');
@@ -18,8 +12,8 @@ var el = this.el; // The entity to which this component is attached.
 var scene = el.sceneEl; // The A-Frame Scene
 
 // var el = document.querySelector('#renderer'); // access to the entity
-var renderer = el.components.renderer.renderer; // access the renderer
-renderer.xr.setSessionMode( 'immersive-vr' ); // activates the VR mode
+// var renderer = el.components.renderer.renderer; // access the renderer
+// renderer.xr.setSessionMode( 'immersive-vr' ); // activates the VR mode
 
 var threeScene = scene.getObject3D('scene'); // Die Three.js-Szene
 
@@ -32,15 +26,3 @@ var camera = document.querySelector('#camera').getObject3D('camera');
 var controls = new PointerLockControls( camera, document.body );
 threeScene.add( controls.getObject() ) // fügt die Steuerung der Szene hinzu
 
-
-function animate() {
-  renderer.render( scene, camera ); // Rendert die Szene
-}
-
-// verwendet requestAnimationFrame, um die animate-Funktion aufzurufen
-function loop() {
-  requestAnimationFrame( loop ); // Ruft die loop-Funktion wieder auf
-  animate(); // Ruft die animate-Funktion auf
-}
-
-loop(); // Startet die loop-Funktion
