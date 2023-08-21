0c95218470d25de3e5c87b591fbe3ef6c967412c
r23 <info@r23.de>
2023-08-21 17:16:38 +0200

    misc update

--- metaverse-experience/src/index.js
+++ metaverse-experience/src/index.js
@@ -3,11 +3,13 @@ import * as AFRAME from 'aframe';
 
 
 import 'aframe-extras/dist/aframe-extras.loaders.min.js'; 
-import 'aframe-blink-controls/dist/aframe-blink-controls.min.js'; 
 import 'aframe-physics-system/dist/aframe-physics-system.min.js';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
+
+
+require('aframe-blink-controls');
 
 // var el = this.el; // The entity to which this component is attached.
 // var scene = el.sceneEl; // The A-Frame Scene
@@ -27,3 +29,31 @@ import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockCont
 // var controls = new PointerLockControls( camera, document.body );
 // threeScene.add( controls.getObject() ) // fügt die Steuerung der Szene hinzu
 
+    // On click, send the NPC to the target location.
+    AFRAME.registerComponent('nav-pointer', {
+        init: function () {
+          const el = this.el;
+      
+          // On click, send the NPC to the target location.
+          el.addEventListener('click', (e) => {
+            const ctrlEl = el.sceneEl.querySelector('[nav-agent]');
+            ctrlEl.setAttribute('nav-agent', {
+              active: true,
+              destination: e.detail.intersection.point
+            });
+          });
+      
+          // When hovering on the nav mesh, show a green cursor.
+          el.addEventListener('mouseenter', () => {
+            el.setAttribute('material', {color: 'green'});
+          });
+          el.addEventListener('mouseleave', () => {
+            el.setAttribute('material', {color: 'crimson'})
+          });
+      
+          // Refresh the raycaster after models load.
+          el.sceneEl.addEventListener('object3dset', () => {
+            this.el.components.raycaster.refreshObjects();
+          });
+        }
+      });
