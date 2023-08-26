bc7d80eb8706fa3cbbcc88752c8810d752520e6f
r23 <info@r23.de>
2023-08-26 21:52:41 +0200

    import webpack splitChunks minSize maxSize

---
+++ metaverse-experience/dist/index.php
@@ -0,0 +1,139 @@
+<html>
+  <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no,user-scalable=no,maximum-scale=1">
+    <title>Web-Based Virtual Experiences in the Metaverse</title>
+
+<script src="static/js/runtime-main.js"></script>
+<script src="static/js/572.js"></script>
+<script src="static/js/734.js"></script>
+<script src="static/js/203.js"></script>
+<script src="static/js/434.js"></script>
+<script src="static/js/454.js"></script>
+<script src="static/js/986.js"></script>
+<script src="static/js/58.js"></script>
+<script src="static/js/710.js"></script>
+<script src="static/js/223.js"></script>
+<script src="static/js/850.js"></script>
+<script src="static/js/848.js"></script>
+<script src="static/js/227.js"></script>
+<script src="static/js/569.js"></script>
+<script src="static/js/687.js"></script>
+<script src="static/js/406.js"></script>
+<script src="static/js/477.js"></script>
+<script src="static/js/main.js"></script>
+    <script src="https://unpkg.com/aframe-environment-component@1.3.2/dist/aframe-environment-component.min.js"></script>
+    <script>
+/**
+ * Basic emissive effect.
+ */
+AFRAME.registerComponent('glow', {
+  schema: {
+    color: {default: '#ffffff', type: 'color'},
+    intensity: {default: 1.0}
+  },
+  init: function () {
+    this.el.addEventListener('object3dset', function () {
+      this.update();
+    }.bind(this));
+  },
+  update: function () {
+    var data = this.data;
+    this.el.object3D.traverse(function (node) {
+      if (node.isMesh) {
+        node.material.emissive.copy(new THREE.Color(data.color));
+        node.material.emissiveIntensity = data.intensity;
+      }
+    });
+  }
+});
+
+/**
+ * Simple spin-and-levitate animation.
+ */
+AFRAME.registerComponent('levitate', {
+  tick: function (t, dt) {
+    var mesh = this.el.getObject3D('mesh');
+    if (!mesh) return;
+    mesh.rotation.y += 0.1 * dt / 1000;
+    mesh.position.y = 0.25 * Math.sin(t / 1000);
+  }
+});
+
+/**
+ * Removes current element if on a mobile device.
+ */
+AFRAME.registerComponent('not-mobile',  {
+  init: function () {
+    var el = this.el;
+    if (el.sceneEl.isMobile) {
+      el.parentEl.remove(el);
+    }
+  }
+});
+    </script>
+  </head>
+<body>
+<?php
+# <a-scene vr-mode-ui="enabled: true"> <!-- creates a UI element for the VR mode -->		
+# <a-scene environment="preset: forest; lighting: point; lightPosition: -5 10 0;">
+?>
+	<a-scene environment="preset: forest; lighting: point; lightPosition: -5 10 0;">
+		<a-assets>
+			<img id="skyTexture" src="texture/kloofendal_43d_clear_puresky.jpg">
+			<a-asset-item id="navmesh" src="model/hall-navmesh.glb"></a-asset-item>
+			<a-asset-item id="hall" src="model/hall.glb"></a-asset-item>
+			<a-asset-item id="gem" src="model/rupee.glb"></a-asset-item>
+		</a-assets>
+	<a-sky src="#skyTexture"></a-sky>
+	
+	
+
+      <!-- Player. -->
+      <a-entity id="rig"
+                movement-controls="constrainToNavMesh: true;
+                                   controls: checkpoint, gamepad, trackpad, keyboard, touch;"
+                position="-7 0 21">
+        <a-entity camera
+                  position="0 1.6 0"
+                  look-controls="pointerLockEnabled: true">
+          <a-cursor></a-cursor>
+        </a-entity>
+      </a-entity>
+
+      <!-- Teleport gems. -->
+      <a-entity id="wall-gem"
+                checkpoint="offset: 0 -0.8 0;"
+                position="-9 7 -6"
+                gltf-model="#gem"
+                scale="0.05 0.05 0.05"
+                glow="color: #33FF66; intensity: 0.5"
+                levitate>
+        <a-light not-mobile type="point" intensity="0.5" color="#33FF66" distance="2" position="0.5 -0.25 0"></a-light>
+      </a-entity>
+      <a-entity id="tower-gem"
+                checkpoint="offset: 0 -0.8 0;"
+                position="0 10.2 -3.5"
+                gltf-model="#gem"
+                scale="0.05 0.05 0.05"
+                glow="color: #33FF66; intensity: 0.5"
+                levitate>
+        <a-light not-mobile type="point" intensity="0.5" color="#33FF66" distance="2" position="0.5 -0.25 0"></a-light>
+      </a-entity>
+
+      <!-- Nav mesh. -->
+      <a-entity nav-mesh
+                normal-material
+                visible="false"
+                position="0 0 20"
+                gltf-model="#navmesh"></a-entity>
+
+      <!-- Scene. -->
+      <a-entity position="0 0 20"
+                scale="1 1 1"
+                gltf-model="#hall">
+      </a-entity>
+    </a-scene>
+  </body>
+</html>
--- metaverse-experience/webpack.config.js
+++ metaverse-experience/webpack.config.js
@@ -1,27 +1,56 @@
 const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
+    mode: 'production', // 'development',
     entry: path.resolve(__dirname, './src/index.js'),
     output: {
-        filename: 'main.js',
+        filename: 'static/js/[name].js', // Use a placeholder for filename
         path: path.resolve(__dirname, 'dist'),
     },
-    target: 'node',
     module: {
         rules: [
             {
                 test: /\.js$/,
-                include: [
-                    path.resolve(__dirname, 'src'),
-                    require.resolve('aframe-physics-system')
-                ],
+                include: [path.resolve(__dirname, 'src'), require.resolve('aframe-physics-system') ],
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
+                  loader: 'babel-loader', // or whatever loader you're using to parse modules
+                  options: {}
                 }
-            }
+              }
+
         ]
-    }
+    },
+    optimization: {
+        splitChunks: {
+          chunks: 'all', // Split all chunks
+          minSize: 20000, // Minimum size of chunk to be generated
+          maxSize: 0, // No maximum size limit
+          minChunks: 1, // Minimum number of chunks that must share a module
+          maxAsyncRequests: 30, // Maximum number of parallel requests
+          maxInitialRequests: 30, // Maximum number of initial requests
+          automaticNameDelimiter: '~', // Delimiter for generated names
+          enforceSizeThreshold: 50000, // Threshold for enforcing chunk size
+          cacheGroups: {
+            defaultVendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              reuseExistingChunk: true,
+            },
+            default: {
+              minChunks: 2,
+              priority: -20,
+              reuseExistingChunk: true,
+            },
+          },
+        },
+        runtimeChunk: {name: (entrypoint) => `runtime-${entrypoint.name}`}, // Use a separate chunk for runtime code
+   },
+   plugins: [
+    new TerserPlugin({
+      terserOptions: {
+        // Use the default options for terser
+      },
+    }),
+  ],
 };
