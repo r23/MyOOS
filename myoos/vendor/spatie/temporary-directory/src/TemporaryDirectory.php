919f20dc48635ecb40e95c67c6934460bfbd83f0
r23 <info@r23.de>
2023-09-25 15:04:22 +0200

    - Upgrading spatie/temporary-directory (2.1.2 => 2.2.0)

--- myoos/vendor/spatie/temporary-directory/src/TemporaryDirectory.php
+++ myoos/vendor/spatie/temporary-directory/src/TemporaryDirectory.php
@@ -15,6 +15,8 @@ class TemporaryDirectory
 
     protected bool $forceCreate = false;
 
+    protected bool $deleteWhenDestroyed = false;
+
     public function __construct(string $location = '')
     {
         $this->location = $this->sanitizePath($location);
@@ -178,8 +180,22 @@ class TemporaryDirectory
             gc_collect_cycles();
 
             return rmdir($path);
-        } catch (Throwable $throwable) {
+        } catch (Throwable) {
             return false;
+        }
+    }
+
+    public function deleteWhenDestroyed(bool $deleteWhenDestroyed = true): self
+    {
+        $this->deleteWhenDestroyed = $deleteWhenDestroyed;
+
+        return $this;
+    }
+
+    public function __destruct()
+    {
+        if ($this->deleteWhenDestroyed) {
+            $this->delete();
         }
     }
 }
