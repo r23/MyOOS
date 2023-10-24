82565c69142e35914de5a087ebd67d37cf2f786a
r23 <info@r23.de>
2023-10-24 04:36:04 +0200

    import WP Super Cache

---
+++ wordpress/wp-content/plugins/wp-super-cache/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/wp-super-cache',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => NULL,
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/jetpack-device-detection' => array(
+            'pretty_version' => 'v1.4.26',
+            'version' => '1.4.26.0',
+            'reference' => '303b06e76ef7976aef7e41423573b53d6860b1df',
+            'type' => 'jetpack-library',
+            'install_path' => __DIR__ . '/../automattic/jetpack-device-detection',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'automattic/wp-super-cache' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => NULL,
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
