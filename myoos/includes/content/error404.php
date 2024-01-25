adac031b0b7e0157b80d4f587d2c298c0528909a
r23 <info@r23.de>
2024-01-25 17:41:12 +0100

    happy new year 2024

--- myoos/includes/content/error404.php
+++ myoos/includes/content/error404.php
@@ -5,7 +5,7 @@
    MyOOS [Shopsystem]
    https://www.oos-shop.de
 
-   Copyright (c) 2003 - 2023 by the MyOOS Development Team.
+   Copyright (c) 2003 - 2024  by the MyOOS Development Team.
    ----------------------------------------------------------------------
    Released under the GNU General Public License
    ----------------------------------------------------------------------
--- myoos/index.php
+++ myoos/index.php
@@ -5,7 +5,7 @@
    MyOOS [Shopsystem]
    https://www.oos-shop.de
 
-   Copyright (c) 2003 - 2023  by the MyOOS Development Team.
+   Copyright (c) 2003 - 2024  by the MyOOS Development Team.
    ----------------------------------------------------------------------
    Based on:
 
